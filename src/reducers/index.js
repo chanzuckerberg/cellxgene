5410a19ce94417d8c6acaceda2c21f4ae8cd0ccc
Colin Megill <colinmegill@gmail.com>
2017-10-09 10:10:28 -0700

    add devtools and url parsing middleware to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,17 @@
-import { combineReducers, createStore } from 'redux'
-import cells from './cells'
+import { combineReducers, createStore, applyMiddleware } from 'redux';
+import updateURLMiddleware from "../middleware/updateURLMiddleware";
+import cells from './cells';
+import onURLchange from "./onURLchange";
 
 const Reducer = combineReducers({
   cells,
+  onURLchange,
 })
 
-let store = createStore(Reducer);
+let store = createStore(
+  Reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(updateURLMiddleware)
+);
 
 export default store;
