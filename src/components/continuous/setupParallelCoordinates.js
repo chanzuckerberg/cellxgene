b54cc122be0865a24a60e79f9b862debe4cb68a7
Colin Megill <colinmegill@gmail.com>
2017-11-06 14:32:49 -0800

    modularize parallel coords setup

---
+++ src/components/continuous/setupParallelCoordinates.js
@@ -0,0 +1,42 @@
+/*****************************************
+******************************************
+      Setup SVG & Canvas elements
+******************************************
+******************************************/
+
+const setupParallelCoordinates = (
+  width,
+  height,
+  margin
+) => {
+
+  var container = d3.select("#parcoords")
+
+  var svg = container.append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var canvas = container.append("canvas")
+      .attr("width", width * devicePixelRatio)
+      .attr("height", height * devicePixelRatio)
+      .style("width", width + "px")
+      .style("height", height + "px")
+      .style("margin-top", margin.top + "px")
+      .style("margin-left", margin.left + "px");
+
+  var ctx = canvas.node().getContext("2d");
+      ctx.globalCompositeOperation = 'darken';
+      ctx.globalAlpha = 0.15;
+      ctx.lineWidth = 1.5;
+      ctx.scale(devicePixelRatio, devicePixelRatio);
+
+  return {
+    svg,
+    ctx,
+  }
+
+}
+
+export default setupParallelCoordinates
