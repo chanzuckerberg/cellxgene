af656649e120cfec438d2081d955c1ab2a0e8de0
Colin Megill <colinmegill@gmail.com>
2017-11-08 16:35:16 -0800

    add continuous selection to reducer

--- src/reducers/controls.js
+++ src/reducers/controls.js
@@ -1,11 +1,17 @@
 const Controls = (state = {
   color: null,
+  continuousSelection: null,
 }, action) => {
   switch (action.type) {
   case "color changed":
     return Object.assign({}, state, {
       color: action.data,
     });
+  case "continuous selection using parallel coords brushing": {
+    return Object.assign({}, state, {
+      continuousSelection: action.data,
+    });
+  }
   default:
     return state;
   }
