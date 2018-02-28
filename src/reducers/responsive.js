8b587da7132103556047a436419260fd52f4c1b1
Colin Megill <colinmegill@gmail.com>
2018-02-28 10:03:19 -0800

    Add responsive reducer

---
+++ src/reducers/responsive.js
@@ -0,0 +1,16 @@
+const Responsive = (state = {
+  width: null,
+  height: null,
+}, action) => {
+  switch (action.type) {
+  case "resize event":
+    return Object.assign({}, state, {
+      width: action.data,
+      height: action.data
+    });
+  default:
+    return state;
+  }
+};
+
+export default Responsive;
