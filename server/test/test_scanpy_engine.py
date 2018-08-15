ed109a0a31b75044a25f173bf5e9b14a826c2939
Charlotte Weaver <charlottesweaver@gmail.com>
2018-08-15 14:08:56 -0700

    Using new filter specs
    Indexing working

--- server/test/test_scanpy_engine.py
+++ server/test/test_scanpy_engine.py
@@ -12,7 +12,7 @@ class UtilTest(unittest.TestCase):
         self.assertEqual(self.data.cell_count, 2638)
         self.assertEqual(self.data.gene_count, 1838)
         epsilon = 0.000005
-        self.assertTrue(self.data.data.X[0,0] - -0.17146951 < epsilon)
+        self.assertTrue(self.data.data.X[0, 0] - -0.17146951 < epsilon)
 
     def test_mandatory_annotations(self):
         self.assertIn("name", self.data.data.obs)
@@ -24,6 +24,20 @@ class UtilTest(unittest.TestCase):
     def test_data_type(self):
         self.data.data.X = self.data.data.X.astype("float64")
         self.assertWarns(UserWarning, self.data._validatate_data_types())
+
+    def test_filter_idx(self):
+        filter = {
+            "filter": {
+                "var": {
+                    "index": [1, 99, [200, 300]]
+                },
+                "obs": {
+                    "index": [1, 99, [1000, 2000]]
+                }
+            }
+        }
+        data = self.data.filter_dataframe(filter["filter"])
+        self.assertEqual(data.shape, (1002, 102))
 
 
 if __name__ == '__main__':
