609aa34f456b301df451734a06d664c08aeac9e9
Charlotte Weaver <charlottesweaver@gmail.com>
2018-09-13 14:29:05 -0700

    Run cellxgene server in test setup

--- server/test/test_api.py
+++ server/test/test_api.py
@@ -1,19 +1,39 @@
 import unittest
 import requests
+from subprocess import Popen
+import time
+
+LOCAL_URL = "http://127.0.0.1:5005/"
+VERSION = "v0.2"
+URL_BASE = f"{LOCAL_URL}api/{VERSION}/"
 
 
 class EndPoints(unittest.TestCase):
     """Test Case for endpoints"""
 
+    @classmethod
+    def setUpClass(cls):
+        cls.ps = Popen(["cellxgene", "scanpy", "example-dataset/"])
+        session = requests.Session()
+        for i in range(90):
+            try:
+                session.get(f"{URL_BASE}schema")
+            except requests.exceptions.ConnectionError:
+                time.sleep(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            cls.ps.terminate()
+        except ProcessLookupError:
+            pass
+
     def setUp(self):
-        # Local
-        self.local_url = "http://127.0.0.1:5005/"
-        self.version = "v0.2"
-        self.url_base = "{local_url}api/{version}/".format(local_url=self.local_url, version=self.version)
         self.session = requests.Session()
 
     def test_initialize(self):
-        url = "{base}{endpoint}".format(base=self.url_base, endpoint="schema")
+        endpoint = "schema"
+        url = f"{URL_BASE}{endpoint}"
         result = self.session.get(url)
         self.assertEqual(result.status_code, 200)
         result_data = result.json()
@@ -21,7 +41,8 @@ class EndPoints(unittest.TestCase):
         self.assertEqual(len(result_data["schema"]["annotations"]["obs"]), 5)
 
     def test_config(self):
-        url = "{base}{endpoint}".format(base=self.url_base, endpoint="config")
+        endpoint = "config"
+        url = f"{URL_BASE}{endpoint}"
         result = self.session.get(url)
         self.assertEqual(result.status_code, 200)
         result_data = result.json()
@@ -29,7 +50,8 @@ class EndPoints(unittest.TestCase):
         self.assertEqual(len(result_data["config"]["features"]), 4)
 
     def test_get_layout(self):
-        url = "{base}{endpoint}".format(base=self.url_base, endpoint="layout/obs")
+        endpoint = "layout/obs"
+        url = f"{URL_BASE}{endpoint}"
         result = self.session.get(url)
         self.assertEqual(result.status_code, 200)
         result_data = result.json()
@@ -37,6 +59,9 @@ class EndPoints(unittest.TestCase):
         self.assertEqual(len(result_data["layout"]["coordinates"]), 2638)
 
     def test_static(self):
-        url = "{url}{endpoint}/{file}".format(url=self.local_url, endpoint="static", file="js/service-worker.js")
+        endpoint = "static"
+        file = "js/service-worker.js"
+        url = f"{LOCAL_URL}{endpoint}/{file}"
         result = self.session.get(url)
+        print(result.status_code)
         self.assertEqual(result.status_code, 200)
