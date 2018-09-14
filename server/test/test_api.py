73a949a663fdd0a2aad2755b3c2b8fef6945444a
Charlotte Weaver <charlottesweaver@gmail.com>
2018-09-14 11:51:40 -0700

    Cleanup Tasks (#216)
    * Add test_api back into travis build
    
    * Do custom JSON encoding the correct way
    
    * Run cellxgene server in test setup
    
    * Cleanup new tests too

--- server/test/test_api.py
+++ server/test/test_api.py
@@ -1,20 +1,40 @@
-import json
 import requests
+from subprocess import Popen
 import unittest
+import time
+
+LOCAL_URL = "http://127.0.0.1:5005/"
+VERSION = "v0.2"
+URL_BASE = f"{LOCAL_URL}api/{VERSION}/"
+
 
 
 class EndPoints(unittest.TestCase):
     """Test Case for endpoints"""
 
+    @classmethod
+    def setUpClass(cls):
+        cls.ps = Popen(["cellxgene", "scanpy", "example-dataset/"])
+        session = requests.Session()
+        for i in range(90):
+            try:
+                session.get(f"{URL_BASE}schema")
+            except requests.exceptions.ConnectionError:
+                time.sleep(1)
+
+    @classmethod
+    def tearDownClass(cls):
+        try:
+            cls.ps.terminate()
+        except ProcessLookupError:
+            pass
+
     def setUp(self):
-        # Local
-        self.local_url = "http://127.0.0.1:5005/"
-        self.version = "v0.2"
-        self.url_base = "{local_url}api/{version}/".format(local_url=self.local_url, version=self.version)
         self.session = requests.Session()
 
     def test_initialize(self):
-        url = "{base}{endpoint}".format(base=self.url_base, endpoint="schema")
+        endpoint = "schema"
+        url = f"{URL_BASE}{endpoint}"
         result = self.session.get(url)
         self.assertEqual(result.status_code, 200)
         result_data = result.json()
@@ -22,7 +42,8 @@ class EndPoints(unittest.TestCase):
         self.assertEqual(len(result_data["schema"]["annotations"]["obs"]), 5)
 
     def test_config(self):
-        url = "{base}{endpoint}".format(base=self.url_base, endpoint="config")
+        endpoint = "config"
+        url = f"{URL_BASE}{endpoint}"
         result = self.session.get(url)
         self.assertEqual(result.status_code, 200)
         result_data = result.json()
@@ -30,39 +51,17 @@ class EndPoints(unittest.TestCase):
         self.assertEqual(len(result_data["config"]["features"]), 4)
 
     def test_get_layout(self):
-        url = "{base}{endpoint}".format(base=self.url_base, endpoint="layout/obs")
+        endpoint = "layout/obs"
+        url = f"{URL_BASE}{endpoint}"
         result = self.session.get(url)
         self.assertEqual(result.status_code, 200)
         result_data = result.json()
         self.assertEqual(result_data["layout"]["ndims"], 2)
         self.assertEqual(len(result_data["layout"]["coordinates"]), 2638)
 
-    def test_get_annotations(self):
-        url = "{base}{endpoint}".format(base=self.url_base, endpoint="annotations/obs")
-        result = self.session.get(url)
-        self.assertEqual(result.status_code, 200)
-        result_data = result.json()
-        self.assertEqual(result_data["names"], ["n_genes", "percent_mito", "n_counts", "louvain", "name"])
-        self.assertEqual(len(result_data["data"]), 2638)
-        self.assertEqual(len(result_data["data"][0]), 6)
-
-    def test_get_annotations_keys(self):
-        url = "{base}{endpoint}?{query}".format(base=self.url_base, endpoint="annotations/obs",
-                                                query="annotation-name=n_genes&annotation-name=percent_mito")
-        result = self.session.get(url)
-        self.assertEqual(result.status_code, 200)
-        result_data = result.json()
-        self.assertEqual(result_data["names"], ["n_genes", "percent_mito"])
-        self.assertEqual(len(result_data["data"][0]), 3)
-
-    def test_get_annotations_error(self):
-        url = "{base}{endpoint}?{query}".format(base=self.url_base, endpoint="annotations/obs",
-                                                query="annotation-name=notakey")
-        result = self.session.get(url)
-        self.assertEqual(result.status_code, 404)
-
     def test_put_layout(self):
-        url = "{base}{endpoint}".format(base=self.url_base, endpoint="layout/obs")
+        endpoint = "layout/obs"
+        url = f"{URL_BASE}{endpoint}"
         obs_filter = {
             "filter": {
                 "obs": {
@@ -79,7 +78,37 @@ class EndPoints(unittest.TestCase):
         result_data = result.json()
         self.assertEqual(len(result_data["layout"]["coordinates"]), 15)
 
-    def test_static(self):
-        url = "{url}{endpoint}/{file}".format(url=self.local_url, endpoint="static", file="js/service-worker.js")
+    def test_get_annotations(self):
+        endpoint = "annotations/obs"
+        url = f"{URL_BASE}{endpoint}"
         result = self.session.get(url)
+        self.assertEqual(result.status_code, 200)
+        result_data = result.json()
+        self.assertEqual(result_data["names"], ["n_genes", "percent_mito", "n_counts", "louvain", "name"])
+        self.assertEqual(len(result_data["data"]), 2638)
+        self.assertEqual(len(result_data["data"][0]), 6)
+
+    def test_get_annotations_keys(self):
+        endpoint = "annotations/obs"
+        query = "annotation-name=n_genes&annotation-name=percent_mito"
+        url = f"{URL_BASE}{endpoint}?{query}"
+        result = self.session.get(url)
+        self.assertEqual(result.status_code, 200)
+        result_data = result.json()
+        self.assertEqual(result_data["names"], ["n_genes", "percent_mito"])
+        self.assertEqual(len(result_data["data"][0]), 3)
+
+    def test_get_annotations_error(self):
+        endpoint = "annotations/obs"
+        query = "annotation-name=notakey"
+        url = f"{URL_BASE}{endpoint}?{query}"
+        result = self.session.get(url)
+        self.assertEqual(result.status_code, 404)
+
+    def test_static(self):
+        endpoint = "static"
+        file = "js/service-worker.js"
+        url = f"{LOCAL_URL}{endpoint}/{file}"
+        result = self.session.get(url)
+        print(result.status_code)
         self.assertEqual(result.status_code, 200)
