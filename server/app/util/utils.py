82e643ecae1ca0f551910c558de479ae7aa7c6a1
Charlotte Weaver <charlottesweaver@gmail.com>
2018-09-14 11:51:40 -0700

    Cleanup Tasks (#216)
    * Add test_api back into travis build
    
    * Do custom JSON encoding the correct way
    
    * Run cellxgene server in test setup
    
    * Cleanup new tests too

--- server/app/util/utils.py
+++ server/app/util/utils.py
@@ -1,7 +1,6 @@
 import json
 
 from numpy import float32, integer
-from flask import make_response, Response
 
 
 class Float32JSONEncoder(json.JSONEncoder):
@@ -11,20 +10,3 @@ class Float32JSONEncoder(json.JSONEncoder):
         elif isinstance(obj, integer):
             return int(obj)
         return json.JSONEncoder.default(self, obj)
-
-
-def make_payload(data, errorcode=200):
-    """
-    Creates JSON respons for requests
-    :param data: json data
-    :param errorcode: http error code
-    :return: flask json repsonse
-    """
-    # Questionable
-    data = json.loads(json.dumps(data, cls=Float32JSONEncoder))
-    return make_response(data, errorcode)
-
-
-def make_streaming_response(data_generator, errorcode=200, content_type="application/json"):
-    # TODO headers
-    return Response(data_generator, status=errorcode, content_type=content_type)
