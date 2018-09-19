4543b002e3f4b5cd22b5a6b3f9d73982d25fe947
bkmartinjr <bruce@chanzuckerberg.com>
2018-09-19 11:53:48 -0700

    lint fixes

--- client/src/components/continuous/drawLinesCanvas.js
+++ client/src/components/continuous/drawLinesCanvas.js
@@ -10,54 +10,46 @@ import renderQueue from "../../util/renderQueue";
 ******************************************
 ******************************************/
 
-const drawLinesCanvas = (
-  ctx,
-  dimensions,
-  xscale,
-  colorAccessor,
-  colorScale
-) => {
-  return d => {
-    ctx.globalAlpha = 0.1;
+const drawLinesCanvas = (ctx, dimensions, xscale) => d => {
+  ctx.globalAlpha = 0.1;
 
-    if (d["__selected__"]) {
-      ctx.strokeStyle = d["__color__"];
-    } else {
+  if (d.__selected__) {
+    ctx.strokeStyle = d.__color__;
+  } else {
+    return;
+  }
+
+  ctx.beginPath();
+  const coords = project(d, dimensions, xscale);
+  coords.forEach((p, i) => {
+    // this tricky bit avoids rendering null values as 0
+    if (p === null) {
+      // this bit renders horizontal lines on the previous/next
+      // dimensions, so that sandwiched null values are visible
+      if (i > 0) {
+        const prev = coords[i - 1];
+        if (prev !== null) {
+          ctx.moveTo(prev[0], prev[1]);
+          ctx.lineTo(prev[0] + 6, prev[1]);
+        }
+      }
+      if (i < coords.length - 1) {
+        const next = coords[i + 1];
+        if (next !== null) {
+          ctx.moveTo(next[0] - 6, next[1]);
+        }
+      }
       return;
     }
 
-    ctx.beginPath();
-    var coords = project(d, dimensions, xscale);
-    coords.forEach((p, i) => {
-      // this tricky bit avoids rendering null values as 0
-      if (p === null) {
-        // this bit renders horizontal lines on the previous/next
-        // dimensions, so that sandwiched null values are visible
-        if (i > 0) {
-          var prev = coords[i - 1];
-          if (prev !== null) {
-            ctx.moveTo(prev[0], prev[1]);
-            ctx.lineTo(prev[0] + 6, prev[1]);
-          }
-        }
-        if (i < coords.length - 1) {
-          var next = coords[i + 1];
-          if (next !== null) {
-            ctx.moveTo(next[0] - 6, next[1]);
-          }
-        }
-        return;
-      }
+    if (i === 0) {
+      ctx.moveTo(p[0], p[1]);
+      return;
+    }
 
-      if (i == 0) {
-        ctx.moveTo(p[0], p[1]);
-        return;
-      }
-
-      ctx.lineTo(p[0], p[1]);
-    });
-    ctx.stroke();
-  };
+    ctx.lineTo(p[0], p[1]);
+  });
+  ctx.stroke();
 };
 
 const drawCellLinesUsingRenderQueue = (
