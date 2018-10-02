9680cf2ed5e518217240abdf04d9166119b15b0e
bkmartinjr <bruce@chanzuckerberg.com>
2018-10-02 13:49:59 -0700

    add Jest config to eslint

--- client/configuration/eslint/eslint.js
+++ client/configuration/eslint/eslint.js
@@ -33,5 +33,23 @@ module.exports = {
     "object-curly-newline": ["error", { consistent: true }],
     "react/prop-types": [0],
     "space-before-function-paren": "off"
-  }
+  },
+  overrides: [
+    {
+      files: ["**/*.test.js"],
+      env: {
+        jest: true // now **/*.test.js files' env has both es6 *and* jest
+      },
+      // Can't extend in overrides: https://github.com/eslint/eslint/issues/8813
+      // "extends": ["plugin:jest/recommended"]
+      plugins: ["jest"],
+      rules: {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
+      }
+    }
+  ]
 };
