8e5c687334e48c657f63dd4815faf7b576072b47
Charlotte Weaver <charlottesweaver@gmail.com>
2018-10-17 15:48:29 -0700

    Do not calculate layout, used saved layout instead
    See for rationale: https://docs.google.com/document/d/1HJFvbdDHxxgkCW0DZzdTZ9CUMgc2ef2rQFukATBQWvE/edit

--- server/app/rest_api/rest.py
+++ server/app/rest_api/rest.py
@@ -636,50 +636,50 @@ class LayoutObsAPI(Resource):
     def get(self):
         return make_response((jsonify({"layout": current_app.data.layout({})})), HTTPStatus.OK)
 
-    @swagger.doc({
-        "summary": "Observation layout for filtered subset.",
-        "tags": ["layout"],
-        "parameters": [
-            {
-                "name": "filter",
-                "description": "Complex Filter",
-                "in": "body",
-                "schema": FilterModel
-            }
-        ],
-        "responses": {
-            "200": {
-                "description": "layout",
-                "examples": {
-                    "application/json": {
-                        "layout": {
-                            "ndims": 2,
-                            "coordinates": [
-                                [0, 0.284483, 0.983744],
-                                [1, 0.038844, 0.739444]
-                            ]
-                        }
-                    }
-                }
-            },
-            "400": {
-                "description": "Malformed filter"
-            },
-            "403": {
-                "description": "Non-interactive request"
-            },
-        }
-    })
-    def put(self):
-        try:
-            filter = request.get_json()["filter"]
-            interactive_limit = current_app.data.features["layout"]["obs"]["interactiveLimit"]
-            layout = current_app.data.layout(filter, interactive_limit=interactive_limit)
-            return make_response(jsonify({"layout": layout}), HTTPStatus.OK)
-        except FilterError as e:
-            return make_response(e.message, HTTPStatus.BAD_REQUEST)
-        except InteractiveError:
-            return make_response("Non-interactive request", HTTPStatus.FORBIDDEN)
+    # @swagger.doc({
+    #     "summary": "Observation layout for filtered subset.",
+    #     "tags": ["layout"],
+    #     "parameters": [
+    #         {
+    #             "name": "filter",
+    #             "description": "Complex Filter",
+    #             "in": "body",
+    #             "schema": FilterModel
+    #         }
+    #     ],
+    #     "responses": {
+    #         "200": {
+    #             "description": "layout",
+    #             "examples": {
+    #                 "application/json": {
+    #                     "layout": {
+    #                         "ndims": 2,
+    #                         "coordinates": [
+    #                             [0, 0.284483, 0.983744],
+    #                             [1, 0.038844, 0.739444]
+    #                         ]
+    #                     }
+    #                 }
+    #             }
+    #         },
+    #         "400": {
+    #             "description": "Malformed filter"
+    #         },
+    #         "403": {
+    #             "description": "Non-interactive request"
+    #         },
+    #     }
+    # })
+    # def put(self):
+    #     try:
+    #         filter = request.get_json()["filter"]
+    #         interactive_limit = current_app.data.features["layout"]["obs"]["interactiveLimit"]
+    #         layout = current_app.data.layout(filter, interactive_limit=interactive_limit)
+    #         return make_response(jsonify({"layout": layout}), HTTPStatus.OK)
+    #     except FilterError as e:
+    #         return make_response(e.message, HTTPStatus.BAD_REQUEST)
+    #     except InteractiveError:
+    #         return make_response("Non-interactive request", HTTPStatus.FORBIDDEN)
 
 
 def get_api_resources():
--- server/app/scanpy_engine/scanpy_engine.py
+++ server/app/scanpy_engine/scanpy_engine.py
@@ -30,7 +30,6 @@ class ScanpyEngine(CXGDriver):
         self.cell_count = self.data.shape[0]
         self.gene_count = self.data.shape[1]
         self._create_schema()
-        self.layout({})
 
     def _create_schema(self):
         self.schema = {
@@ -340,7 +339,9 @@ class ScanpyEngine(CXGDriver):
         # TODO Filtering cells is fine, but filtering genes does nothing because the neighbors are
         # calculated using the original vars (geneset) and this doesn’t get updated when you use less.
         # Need to recalculate neighbors (long) if user requests new layout filtered by var
-        getattr(sc.tl, self.layout_method)(df, random_state=123)
+        # TODO for MVP we are pushing computation of layout to preprocessing and not allowing re-layout
+        # this will probably change after user feedback
+        # getattr(sc.tl, self.layout_method)(df, random_state=123)
         df_layout = df.obsm[f"X_{self.layout_method}"]
         normalized_layout = DataFrame((df_layout - df_layout.min()) / (df_layout.max() - df_layout.min()),
                                       index=df.obs.index)
