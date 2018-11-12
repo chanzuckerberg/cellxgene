6d39be5275171d151b59c9e61286dab9c1ad39fa
bkmartinjr <bruce@chanzuckerberg.com>
2018-11-12 13:33:07 -0800

    first draft of fast diffexp

---
+++ server/app/scanpy_engine/diffexp.py
@@ -0,0 +1,73 @@
+
+import numpy as np
+from scipy import sparse, stats
+
+# Convenience function which handles sparse data
+def _mean_var_n(X):
+    n = X.shape[0]
+    if sparse.issparse(X):
+        mean = X.mean(axis=0).A1
+        mean_sq = X.multiply(X).mean(axis=0).A1
+    else:
+        mean = X.mean(axis=0)
+        mean_sq = np.multiply(X, X).mean(axis=0)
+
+    v = (mean_sq - mean**2) * (n / (n-1))
+    return mean, v, n
+
+def diffexp_ttest(adata, maskA, maskB, top_n=8):
+    """
+    Return differential expression statistics for top N variables, sorted by
+    t statistic.   Implemented as a unequal variance t-test.
+
+    :param adata: anndata dataframe
+    :param maskA: observation selection mask for set 1
+    :param maskB: observation selection mask for set 2
+    :param top_n: number of variables to return stats for
+    :return:  for top N genes, [ varindex, logfoldchange, pval, pval_adj ]
+    """
+
+    # mean, variance, N
+    meanA, vA, nA = _mean_var_n(adata.X[maskA])
+    meanB, vB, nB = _mean_var_n(adata.X[maskB])
+
+    # variance / N
+    vnA = vA / nA
+    vnB = vB / nB
+    sum_vn = vnA + vnB
+
+    # degrees of freedom for Welch's t-test
+    with np.errstate(divide='ignore', invalid='ignore'):
+        dof = sum_vn**2 / (vnA**2 / (nA - 1) + vnB**2 / (nB - 1))
+    dof[np.isnan(dof)] = 1
+
+    # Welch's t-test score calculation
+    with np.errstate(divide='ignore', invalid='ignore'):
+        tscores = (meanA - meanB) / np.sqrt(sum_vn)
+    tscores[np.isnan(tscores)] = 0
+
+    # p-value
+    pvals = stats.t.sf(np.abs(tscores), dof) * 2
+    pvals_adj = pvals * adata.X.shape[1]
+
+    # logfoldchanges: log2(meanA / meanB)
+    logfoldchanges = np.log2(np.abs((meanA + 1e-9) / (meanB + 1e-9)))
+
+    # top n sort
+    stats_to_sort = np.abs(tscores)
+    partition = np.argpartition(stats_to_sort, -top_n)[-top_n:]
+    rel_sort_order = np.argsort(stats_to_sort[partition])[::-1]
+    vars_indices = np.arange(adata.n_vars, dtype=int)
+    sort_order = vars_indices[partition][rel_sort_order]
+
+    # top n slice
+    logfoldchanges_top_n = logfoldchanges[sort_order]
+    pvals_top_n = pvals[sort_order]
+    pvals_adj_top_n = pvals_adj[sort_order]
+
+    # varIndex, logfoldchange, pval, pval_adj
+    result = [ [sort_order[i],
+                logfoldchanges_top_n[i],
+                pvals_top_n[i],
+                pvals_adj_top_n[i] ] for i in range(top_n)]
+    return result
