0ef6c36f4c732378ddde68415ab0e0e5d495a1b6
Colin Megill <colinmegill@gmail.com>
2018-12-17 14:19:48 -0500

    Cluster Occupancy (n dim cube) (#513)
    * create occupancy component
    
    * occupancy working
    
    * centering, width, flex spacing
    
    * add is-number
    
    * extend sort to cover string + int
    
    * proof of concept sorted occupancy
    
    * handle undefined occupancy entry, cleanup
    
    * only render occupancy when colorby is cat
    
    * cleanup unused vars
    
    * cleanup

--- client/src/components/categorical/util.js
+++ client/src/components/categorical/util.js
@@ -3,9 +3,33 @@
 // values is [ [optVal, optIdx], ...]
 // index is range array
 // return sorted index
-export default values =>
-  values.sort((a, b) => {
+
+import isNumber from "is-number";
+import _ from "lodash";
+
+const sortedCategoryValues = values => {
+  /* this sort could be memoized for perf */
+
+  const strings = [];
+  const ints = [];
+
+  _.forEach(values, v => {
+    if (isNumber(v[0])) {
+      ints.push(v);
+    } else {
+      strings.push(v);
+    }
+  });
+
+  strings.sort((a, b) => {
     const textA = String(a[0]).toUpperCase();
     const textB = String(b[0]).toUpperCase();
     return textA < textB ? -1 : textA > textB ? 1 : 0;
   });
+
+  ints.sort((a, b) => +a[0] - +b[0]);
+
+  return ints.concat(strings);
+};
+
+export default sortedCategoryValues;
