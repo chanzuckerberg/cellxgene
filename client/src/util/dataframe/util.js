547abb34a6c08ca83cc57ed02af07fb6f86b7c85
bkmartinjr <bruce@chanzuckerberg.com>
2019-02-08 10:48:49 -0800

    add callOnceLazy

--- client/src/util/dataframe/util.js
+++ client/src/util/dataframe/util.js
@@ -12,3 +12,17 @@ export function isTypedArray(x) {
 export function isArrayOrTypedArray(x) {
   return Array.isArray(x) || isTypedArray(x);
 }
+
+export function callOnceLazy(f) {
+  let value;
+  let calledOnce = false;
+  const result = function result(...args) {
+    if (!calledOnce) {
+      value = f(...args);
+      calledOnce = true;
+    }
+    return value;
+  };
+
+  return result;
+}
