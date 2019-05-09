1da6e0d62154330df7b708cff42136f6c2659cf8
Charlotte Weaver <charlottesweaver@gmail.com>
2019-05-09 16:26:21 -0700

    pr review suggestions

--- server/utils/utils.py
+++ server/utils/utils.py
@@ -1,34 +1,32 @@
-import click
-
+import contextlib
+import errno
+import socket
 
 def find_available_port(host, port):
     """
     Helper method to find open port on host. Tries 50 ports incremented from the specified port
     """
-    import errno
-    import socket
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     new_port = port
     found_port = False
     num_ports_to_try = 50
-    for port_to_try in range(port, port + num_ports_to_try):
-        new_port = port + i
-        while True:
-            try:
-                s.bind((host, new_port))
-                found_port = True
+    with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
+        for port_to_try in range(port, port + num_ports_to_try):
+            while True:
+                try:
+                    s.bind((host, port_to_try))
+                    found_port = True
+                    new_port = port_to_try
+                    break
+                except socket.error as e:
+                    # Reraise if error is not "address in use"
+                    if e.errno != errno.EADDRINUSE:
+                        raise e
+                    break
+            if found_port:
                 break
-            except socket.error as e:
-                # Reraise if error is not "address in use"
-                if e.errno != errno.EADDRINUSE:
-                    raise e
-                break
-        if found_port:
-            break
     if not found_port:
-        click.echo(f"[cellxgene] No port in range {new_port} - {new_port + num_ports_to_try - 1} was"
+        print(f"[cellxgene] No port in range {new_port} - {new_port + num_ports_to_try - 1} was"
                    f" available to run cellxgene server on.")
-        click.echo(f"[cellxgene] Exiting")
+        print(f"[cellxgene] Exiting")
         raise OSError("No available ports")
-    s.close()
     return new_port
