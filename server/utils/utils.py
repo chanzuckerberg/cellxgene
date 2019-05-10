f7b88978d0d3fb5e381ee30f3c6c62fd82ec9195
Charlotte Weaver <charlottesweaver@gmail.com>
2019-05-09 17:23:31 -0700

    simplify find_available_port function

--- server/utils/utils.py
+++ server/utils/utils.py
@@ -7,24 +7,19 @@ def find_available_port(host, port):
     """
     Helper method to find open port on host. Tries 50 ports incremented from the specified port
     """
-    found_port = False
     num_ports_to_try = 50
     for port_to_try in range(port, port + num_ports_to_try):
         with contextlib.closing(socket.socket(socket.AF_INET, socket.SOCK_STREAM)) as s:
             while True:
                 try:
                     s.bind((host, port_to_try))
-                    found_port = True
                     return port_to_try
                 except socket.error as e:
                     # Reraise if error is not "address in use"
                     if e.errno != errno.EADDRINUSE:
                         raise e
                     break
-            if found_port:
-                break
-    if not found_port:
-        print(f"[cellxgene] No port in range {port} - {port + num_ports_to_try - 1} was"
-              f" available to run cellxgene server on.")
-        print(f"[cellxgene] Exiting")
-        raise OSError("No available ports")
+    print(f"[cellxgene] No port in range {port} - {port + num_ports_to_try - 1} was"
+          f" available to run cellxgene server on.")
+    print(f"[cellxgene] Exiting")
+    raise OSError("No available ports")
