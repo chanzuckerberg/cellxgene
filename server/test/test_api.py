0c8fdc07324e763032deb94e1b2ec4a4febfbf76
Charlotte Weaver <charlottesweaver@gmail.com>
2019-05-13 15:01:22 -0700

    fix tests

--- server/test/test_api.py
+++ server/test/test_api.py
@@ -19,7 +19,7 @@ class EndPoints(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.ps = Popen(["cellxgene", "launch", "example-dataset/pbmc3k.h5ad", "--debug", "--no-port-scan"])
+        cls.ps = Popen(["cellxgene", "launch", "example-dataset/pbmc3k.h5ad", "--debug", "--port", "5005"])
         session = requests.Session()
         for i in range(90):
             try:
