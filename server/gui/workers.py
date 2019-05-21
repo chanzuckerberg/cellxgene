9600b14cbb6149b90433cc5d29eb9deabc90376b
Charlotte Weaver <charlottesweaver@gmail.com>
2019-05-21 14:11:42 -0700

    worker multiprocess

--- server/gui/workers.py
+++ server/gui/workers.py
@@ -1,22 +1,31 @@
+from multiprocessing import Process
 import traceback
 
-from server.gui.utils import WorkerSignals
+s
+class EmittingProcess(Process):
+    def __init__(self, transport, *arg, **kwargs):
+        super(EmittingProcess, self).__init__()
+        self.transport = transport
+
+    def emit(self, signal_name, *args):
+        message = (signal_name, *args)
+        self.transport.send(message)
 
 
-class DataLoadWorker():
-    def __init__(self, data_file, layout="umap", *args, **kwargs):
-        super(DataLoadWorker, self).__init__()
+class Worker(EmittingProcess):
+    def __init__(self, transport, data_file, title, host, port, layout=["umap"], *args, **kwargs):
+        super(Worker, self).__init__()
         self.data_file = data_file
         self.layout = layout
-        self.signals = WorkerSignals()
+        self.title = title
+        self.host = host
+        self.port = port
 
     def run(self):
-        if not self.data_file:
-            self.signals.finished.emit()
-            return
-
-        # delayed import to speed load
+        from server.app.app import Server
         from server.app.scanpy_engine.scanpy_engine import ScanpyEngine
+        server = Server()
+        app = server.create_app()
         args = {
             "layout": self.layout,
             "diffexp": "ttest",
@@ -25,15 +34,11 @@ class DataLoadWorker():
             "obs_names": None,
             "var_names": None,
         }
-        try:
-            data_results = ScanpyEngine(self.data_file, args)
-        except Exception as e:
-            traceback.print_exc()
-            self.signals.error.emit(str(e))
-        else:
-            self.signals.result.emit(data_results)
-        finally:
-            self.signals.finished.emit()
+        data = ScanpyEngine(self.data_file, args)
+        server.attach_data(data, self.title)
+        self.emit("ready")
+        # TODO listen for finished to kill?
+        server.app.run(host=self.host, debug=True, port=self.port, threaded=True)
 
 
 class ServerRunWorker():
