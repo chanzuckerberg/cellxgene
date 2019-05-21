6a15cf0fa4e1941987d89ffba66515f534ce9b94
Charlotte Weaver <charlottesweaver@gmail.com>
2019-05-21 14:11:27 -0700

    add emitter

--- server/gui/utils.py
+++ server/gui/utils.py
@@ -13,13 +13,35 @@ class WorkerSignals(QObject):
     Defines the signals available from a running worker thread.
     Supported signals are:
         finished
+        ready
         error - `str` error message
         result - `object` data returned from processing, anything
     """
     finished = Signal()
     error = Signal(str)
     result = Signal(object)
+    ready = Signal()
 
 
 class FileLoadSignals(QObject):
     selectedFile = Signal(str)
+
+class Emitter:
+    def __init__(self, transport, signals):
+        self.transport = transport
+        self.signals = signals()
+
+    def _emit(self, signature, args=None):
+        if args:
+            getattr(self.signals, signature).emit(args)
+        else:
+            getattr(self.signals, signature).emit()
+
+    def run(self):
+        while True:
+            try:
+                signature = self.transport.recv()
+            except Exception:
+                break
+            else:
+                self._emit(*signature)
