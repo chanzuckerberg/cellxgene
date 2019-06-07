82131974d76f351fae816240404bf11530be12ae
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2019-06-07 13:20:34 -0700

    Add id and styling

--- client/src/components/graph/setupCentroidSVG.js
+++ client/src/components/graph/setupCentroidSVG.js
@@ -1,4 +1,5 @@
 import * as d3 from "d3";
+import styles from "./graph.css";
 
 export default (
   responsive,
@@ -7,9 +8,11 @@ export default (
   const svg = d3
   .select("#graphAttachPoint")
   .append("svg")
+  .attr("id", "centroid")
   .attr("data-testid", "centroid-overlay")
   .attr("width", responsive.width - graphPaddingRight)
-  .attr("height", responsive.height);
-
+  .attr("height", responsive.height)
+  .attr("class", `${styles.graphSVG}`);
+//  TODO: Create own styles, ask Colin for an explanation on the css
   return svg;
 }
