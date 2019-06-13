505ba90fb1e737f59e30510c888cfe17146a6c24
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2019-06-13 09:56:54 -0700

    Add finite check

--- client/src/util/centroid.js
+++ client/src/util/centroid.js
@@ -28,21 +28,27 @@ const calcMeanCentroid = (world, annoName, annoValue, layoutDimNames) => {
 const calcMedianCentroid = (world, annoName, annoValue, layoutDimNames) => {
   const centroidX = [];
   const centroidY = [];
+  let hasFinite = false;
+
   const annoArray = world.obsAnnotations.col(annoName).asArray();
   const layoutXArray = world.obsLayout.col(layoutDimNames[0]).asArray();
   const layoutYArray = world.obsLayout.col(layoutDimNames[1]).asArray();
 
   for (let i = 0, len = annoArray.length; i < len; i += 1) {
     if (annoArray[i] === annoValue) {
+      hasFinite = Number.isFinite(annoArray[i]) ? true : hasFinite;
       centroidX.push(layoutXArray[i]);
       centroidY.push(layoutYArray[i]);
     }
   }
+  if (hasFinite) {
+    const medianX = quantile([0.5], Float64Array.from(centroidX));
+    const medianY = quantile([0.5], Float64Array.from(centroidY));
 
-  const medianX = quantile([0.5], Float64Array.from(centroidX));
-  const medianY = quantile([0.5], Float64Array.from(centroidY));
+    return [medianX, medianY];
+  }
 
-  return [medianX, medianY];
+  return [0.5, 0.5];
 };
 
 export default calcMedianCentroid;
