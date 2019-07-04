134fcc899fac97ab6fc27b8112336d5abbe882aa
Colin Megill <colinmegill@gmail.com>
2019-07-03 23:43:22 -0700

    add modal

--- client/src/components/categorical/categorical.js
+++ client/src/components/categorical/categorical.js
@@ -1,7 +1,15 @@
 // jshint esversion: 6
 import React from "react";
 import _ from "lodash";
-import { Button, Tooltip, ControlGroup, InputGroup } from "@blueprintjs/core";
+import {
+  Button,
+  Tooltip,
+  InputGroup,
+  Dialog,
+  Classes,
+  MenuItem
+} from "@blueprintjs/core";
+import { Select } from "@blueprintjs/select";
 import { connect } from "react-redux";
 import * as globals from "../../globals";
 import Category from "./category";
@@ -14,36 +22,47 @@ class Categories extends React.Component {
     super(props);
     this.state = {
       createAnnoModeActive: false,
-      newCategoryText: ""
+      newCategoryText: "",
+      categoryToDuplicate: null
     };
   }
 
   handleCreateUserAnno = () => {
     const { dispatch } = this.props;
-    const { newCategoryText } = this.state;
+    const { newCategoryText, categoryToDuplicate } = this.state;
     dispatch({
       type: "new user annotation category created",
-      data: newCategoryText
+      data: newCategoryText,
+      categoryToDuplicate
     });
     this.setState({
       createAnnoModeActive: false,
+      categoryToDuplicate: null,
       newCategoryText: ""
     });
   };
 
-  handleAnnoModeChange = () => {
-    console.log("anno mode changed");
+  handleEnableAnnoMode = () => {
     this.setState({ createAnnoModeActive: true });
   };
 
-  handleCancelAnnoMode = () => {
+  handleDisableAnnoMode = () => {
     this.setState({ createAnnoModeActive: false });
   };
 
+  handleModalDuplicateCategorySelection = d => {
+    console.log("to duplicate", d);
+    this.setState({ categoryToDuplicate: d });
+  };
+
   render() {
-    const { createAnnoModeActive } = this.state;
+    const { createAnnoModeActive, categoryToDuplicate } = this.state;
     const { categoricalSelection } = this.props;
     if (!categoricalSelection) return null;
+
+    console.log("state", this.state);
+
+    const allCategoryNames = Object.keys(categoricalSelection);
 
     return (
       <div
@@ -66,30 +85,15 @@ class Categories extends React.Component {
             isUserAnno={catState.isUserAnno}
           />
         ))}
-        {!createAnnoModeActive ? (
-          <Tooltip
-            content="Create new categorical metadata field"
-            position="bottom"
-          >
-            <Button
-              style={{ marginTop: 10 }}
-              data-testclass="createAnnoMode"
-              data-testid="createAnnoMode"
-              onClick={this.handleAnnoModeChange}
-              intent="primary"
-              icon="tag"
-            >
-              Create new categorical field
-            </Button>
-          </Tooltip>
-        ) : (
-          <form
-            onSubmit={e => {
-              e.preventDefault();
-              this.handleCreateUserAnno();
-            }}
-          >
-            <ControlGroup style={{ marginTop: 10 }}>
+        <Dialog
+          icon="tag"
+          title="Create new category"
+          isOpen={createAnnoModeActive}
+          onClose={this.handleDisableAnnoMode}
+        >
+          <div className={Classes.DIALOG_BODY}>
+            <div style={{ marginBottom: 20 }}>
+              <p>New, unique category name:</p>
               <InputGroup
                 autoFocus
                 onChange={e =>
@@ -97,25 +101,43 @@ class Categories extends React.Component {
                 }
                 leftIcon="tag"
               />
+            </div>
+            <p>
+              Optionally duplicate all labels & cell assignments from existing
+              category into new category:
+            </p>
+            <Select
+              items={allCategoryNames}
+              filterable={false}
+              itemRenderer={(d, { handleClick }) => {
+                return <MenuItem onClick={handleClick} key={d} text={d} />;
+              }}
+              noResults={<MenuItem disabled text="No results." />}
+              onItemSelect={d => {
+                this.handleModalDuplicateCategorySelection(d);
+              }}
+            >
+              {/* children become the popover target; render value here */}
               <Button
-                data-testclass="createUserAnno"
-                data-testid="createUserAnno"
-                onClick={this.handleCreateUserAnno}
-                intent="primary"
-              >
-                Add Category
+                text={"None (all cells 'unassigned')"}
+                rightIcon="double-caret-vertical"
+              />
+            </Select>
+          </div>
+          <div className={Classes.DIALOG_FOOTER}>
+            <div className={Classes.DIALOG_FOOTER_ACTIONS}>
+              <Tooltip content="Close this dialog without creating a category.">
+                <Button onClick={this.handleDisableAnnoMode}>Cancel</Button>
+              </Tooltip>
+              <Button onClick={this.handleCreateUserAnno} intent="primary">
+                Create new category
               </Button>
-              <Button
-                data-testclass="cancelAnnoMode"
-                data-testid="cancelAnnoMode"
-                onClick={this.handleCancelAnnoMode}
-                intent="default"
-              >
-                Cancel
-              </Button>
-            </ControlGroup>
-          </form>
-        )}
+            </div>
+          </div>
+        </Dialog>
+        <Button onClick={this.handleEnableAnnoMode} intent="primary">
+          Create new category
+        </Button>
       </div>
     );
   }
