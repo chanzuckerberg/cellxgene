9171f2157742e211f519bb6ca632ca17c7319dfb
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2019-07-30 16:23:48 -0700

    create label for each thing

--- client/src/components/graph/setupCentroidSVG.js
+++ client/src/components/graph/setupCentroidSVG.js
@@ -1,7 +1,7 @@
 import * as d3 from "d3";
 import styles from "./graph.css";
 
-export default (responsive, graphPaddingRight, xy, text, colorBy) => {
+export default (responsive, graphPaddingRight, labels, colorBy) => {
   const containerWidth = responsive.width - graphPaddingRight;
 
   const svg = d3
@@ -17,18 +17,20 @@ export default (responsive, graphPaddingRight, xy, text, colorBy) => {
   //  TODO: Create own styles, ask Colin for an explanation on the css
   // For now I'm going to put centroid z-index at 998 and lasso on 999
 
-  const label = svg
-    .append("g")
-    .attr("transform", `translate(${xy[0]}, ${xy[1]})`);
+  for (let i = 0, { length } = labels; i < length; i += 1) {
+    const label = svg
+      .append("g")
+      .attr("transform", `translate(${labels[i][1]}, ${labels[i][2]})`);
 
-  label
-    .append("text")
-    .attr("text-anchor", "middle")
-    .text(text)
-    .style("font-family", "Roboto Condensed")
-    .style("font-size", "18px")
-    .style("font-weight", "700")
-    .style("fill", colorBy ? "black" : "rgb(32, 178, 212)");
+    label
+      .append("text")
+      .attr("text-anchor", "middle")
+      .text(labels[i][0])
+      .style("font-family", "Roboto Condensed")
+      .style("font-size", "18px")
+      .style("font-weight", "700")
+      .style("fill", colorBy ? "black" : "rgb(32, 178, 212)");
+  }
 
   return svg;
 };
