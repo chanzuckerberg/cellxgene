50138527d10c93e47f05bb928e268ce8d5668ac6
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2019-08-02 14:46:40 -0700

    clean up and document

--- client/src/util/centroid.js
+++ client/src/util/centroid.js
@@ -8,7 +8,7 @@ import { memoize } from "./dataframe/util";
 /* Unused - please cleanup
 const calcMeanCentroid = (world, annoName, annoValue, layoutDimNames) => {
   const centroid = { x: 0, y: 0, size: 0 };
-  const annoArray = world.obsAnnotations.col(annoName).asArray();
+  const annoArray = world.obsAnnotations.col(categoryName).asArray();
   const layoutXArray = world.obsLayout.col(layoutDimNames[0]).asArray();
   const layoutYArray = world.obsLayout.col(layoutDimNames[1]).asArray();
 
@@ -29,32 +29,45 @@ const calcMeanCentroid = (world, annoName, annoValue, layoutDimNames) => {
 };
 */
 
+/* 
+  calcMedianCentroid goes through a given metadata category
+  fetches each cell's coordinates grouping by category value.
+
+  It then calculates the median value and puts that in the array
+*/
+
 const calcMedianCentroid = (
   world,
-  annoName,
+  categoryName,
   layoutDimNames,
   categoricalSelection
 ) => {
-  const annoArray = world.obsAnnotations.col(annoName).asArray();
+  const categoryArray = world.obsAnnotations.col(categoryName).asArray();
   const layoutXArray = world.obsLayout.col(layoutDimNames[0]).asArray();
   const layoutYArray = world.obsLayout.col(layoutDimNames[1]).asArray();
+  const coordinates = new Map();
+  // Iterate over all the cells in the category
+  for (let i = 0, len = categoryArray.length; i < len; i += 1) {
+    const categoryValue = categoryArray[i];
 
-  const coordinates = new Map(); // map: annoValue -> [hasFinite, counter, [x Float32], [y Float32]]
-  for (let i = 0, len = annoArray.length; i < len; i += 1) {
-    const categoryValue = annoArray[i];
-
+    // Get the index of the categoryValue within the category
     const categoryValueIndex = categoricalSelection[
-      annoName
+      categoryName
     ].categoryValueIndices.get(categoryValue);
 
-    const numInAnno =
-      categoricalSelection[annoName].categoryValueCounts[categoryValueIndex];
+    // Get the number of cells which are in the category value
+    const numInCategoryValue =
+      categoricalSelection[categoryName].categoryValueCounts[
+        categoryValueIndex
+      ];
 
+    // Create/fetcht the valueArray,
+    // which is what the key points to in the `coordinates` hashmap
     const valueArray = coordinates.get(categoryValue) || [
-      false,
-      0,
-      new Float32Array(numInAnno),
-      new Float32Array(numInAnno)
+      false, // hasFinte
+      0, // index
+      new Float32Array(numInCategoryValue), // x coorindates
+      new Float32Array(numInCategoryValue) // y coorindates
     ];
     const index = valueArray[1];
     let hasFinite = valueArray[0];
@@ -69,6 +82,7 @@ const calcMedianCentroid = (
     coordinates.set(categoryValue, valueArray);
   }
 
+  // Iterate over the recently created map
   const iter = coordinates.entries();
   let pair = iter.next().value;
   let value;
@@ -76,23 +90,31 @@ const calcMedianCentroid = (
   while (pair) {
     key = pair[0];
     value = pair[1];
+    // If there is a finite coordinate for the category value
     if (value[0]) {
+      // Find the median x and y coordinate
+      // and insert them into the first two indices
       value[0] = quantile([0.5], value[2])[0];
       value[1] = quantile([0.5], value[3])[0];
+      // Remove the last two elements (where the arrays of coordinates were)
       value.pop();
       value.pop();
     } else {
+      // remove the entry if not
       coordinates.delete(key);
     }
     pair = iter.next().value;
   }
-
+  // return the map: categoricalValue -> [medianXCoordinate, medianYCoordinate]
   return coordinates;
 };
 
-const hashMedianCentroid = (world, annoName, layoutDimNames) => {
+// A simple function to hash the parameters
+// (not 100% on world hash, Bruce will have to check this one out)
+const hashMedianCentroid = (world, categoryName, layoutDimNames) => {
   return `${world.varAnnotations.__id}+${world.obsLayout.__id}+${
     world.varData.__id
-  }::${annoName}:${layoutDimNames}`;
+  }::${categoryName}:${layoutDimNames}`;
 };
+// export the mmemoized calculation function
 export default memoize(calcMedianCentroid, hashMedianCentroid);
