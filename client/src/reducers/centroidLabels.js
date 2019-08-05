c295c86f851bc8157ebd3c975a4b53eb81089af0
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2019-08-05 15:51:48 -0700

    recalc centroids on world change

--- client/src/reducers/centroidLabels.js
+++ client/src/reducers/centroidLabels.js
@@ -1,26 +1,25 @@
 import calcCentroid from "../util/centroid";
 
-/* const initialState = {
-  metadataField: "",
-  categoryIndex: -1,
-  categoryField: "",
-  centroidXY: [-1, -1]
-}; */
-
 const initialState = {
   labeledCategory: "",
   labels: []
 };
 
 const centroidLabels = (state = initialState, action, sharedNextState) => {
-  /* const { categoricalSelection, world, layoutChoice } = sharedNextState;
-  const { metadataField, categoryIndex } = action;
-  const categoryField =
-    categoricalSelection?.[metadataField]?.categoryValues[categoryIndex]; */
   const { world, layoutChoice, categoricalSelection } = sharedNextState;
   const { metadataField } = action;
 
   switch (action.type) {
+    case "set World to current selection":
+      return {
+        ...state,
+        labels: calcCentroid(
+          world,
+          state.labeledCategory,
+          layoutChoice.currentDimNames,
+          categoricalSelection
+        )
+      };
     case "show centroid labels for category":
       if (state.labeledCategory === metadataField) {
         return initialState;
@@ -35,28 +34,6 @@ const centroidLabels = (state = initialState, action, sharedNextState) => {
           categoricalSelection
         )
       };
-    /*     case "category value mouse hover start":
-      return {
-        ...state,
-        metadataField,
-        categoryIndex,
-        categoryField,
-        centroidXY: calcCentroid(  //This function call is computationally heavy and also leading to large GC. Before reimplementation, look into optimization and memoization
-          world,
-          metadataField,
-          categoryField,
-          layoutChoice.currentDimNames
-        )
-      };
-
-    case "category value mouse hover end":
-      if (
-        metadataField === state.metadataField &&
-        categoryIndex === state.categoryIndex
-      ) {
-        return initialState;
-      }
-      return state; */
 
     default:
       return state;
