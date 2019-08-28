78a43402cb0c1beca5269b3970d4cc31615e4664
Bruce Martin <bruce@chanzuckerberg.com>
2019-08-28 09:11:10 -0700

    package dep update (#905)

--- client/configuration/webpack/webpack.config.prod.js
+++ client/configuration/webpack/webpack.config.prod.js
@@ -40,9 +40,10 @@ module.exports = {
           {
             loader: "css-loader",
             options: {
-              modules: true,
-              importLoaders: 1,
-              localIdentName: "[name]__[local]___[hash:base64:5]"
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]"
+              },
+              importLoaders: 1
             }
           }
         ]
