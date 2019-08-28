a53ed0c44a544056dbe1d812442ad6b7af4a6fc7
bkmartinjr <bruce@chanzuckerberg.com>
2019-08-27 17:52:35 -0700

    package dep update

--- client/configuration/webpack/webpack.config.dev.js
+++ client/configuration/webpack/webpack.config.dev.js
@@ -37,8 +37,9 @@ module.exports = {
           {
             loader: "css-loader",
             options: {
-              modules: true,
-              localIdentName: "[name]__[local]___[hash:base64:5]"
+              modules: {
+                localIdentName: "[name]__[local]___[hash:base64:5]"
+              }
             }
           }
         ]
