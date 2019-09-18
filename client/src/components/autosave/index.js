81e57afe293f621dd21a4b50f763bf4be736de08
Colin Megill <colinmegill@gmail.com>
2019-09-18 19:22:51 -0400

    Anno fixes (easy) (#925)
    * change wording from value to label
    
    * tooltip position
    
    * Padding for text & code whitespace

--- client/src/components/autosave/index.js
+++ client/src/components/autosave/index.js
@@ -4,78 +4,78 @@ import { connect } from "react-redux";
 import actions from "../../actions";
 
 @connect(state => ({
-	universe: state.universe,
-	obsAnnotations: state.universe.obsAnnotations,
-	saveInProgress: state.autosave?.saveInProgress ?? false,
-	lastSavedObsAnnotations: state.autosave?.lastSavedObsAnnotations,
-	error: state.autosave?.error,
-	writableCategoriesEnabled: state.config?.parameters?.["label_file"] ?? false
+  universe: state.universe,
+  obsAnnotations: state.universe.obsAnnotations,
+  saveInProgress: state.autosave?.saveInProgress ?? false,
+  lastSavedObsAnnotations: state.autosave?.lastSavedObsAnnotations,
+  error: state.autosave?.error,
+  writableCategoriesEnabled: state.config?.parameters?.["label_file"] ?? false
 }))
 class Autosave extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			timer: null
-		};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      timer: null
+    };
+  }
 
-	componentDidMount() {
-		const { writableCategoriesEnabled } = this.props;
+  componentDidMount() {
+    const { writableCategoriesEnabled } = this.props;
 
-		let { timer } = this.state;
-		if (timer) clearInterval(timer);
-		if (writableCategoriesEnabled) {
-			timer = setInterval(this.tick, 2500);
-		} else {
-			timer = null;
-		}
-		this.setState({ timer });
-	}
+    let { timer } = this.state;
+    if (timer) clearInterval(timer);
+    if (writableCategoriesEnabled) {
+      timer = setInterval(this.tick, 2500);
+    } else {
+      timer = null;
+    }
+    this.setState({ timer });
+  }
 
-	componentWillUnmount() {
-		const { timer } = this.state;
-		if (timer) this.clearInterval(timer);
-	}
+  componentWillUnmount() {
+    const { timer } = this.state;
+    if (timer) this.clearInterval(timer);
+  }
 
-	tick = () => {
-		const { dispatch, saveInProgress } = this.props;
-		if (this.needToSave() && !saveInProgress) {
-			dispatch(actions.saveObsAnnotations());
-		}
-	};
+  tick = () => {
+    const { dispatch, saveInProgress } = this.props;
+    if (this.needToSave() && !saveInProgress) {
+      dispatch(actions.saveObsAnnotations());
+    }
+  };
 
-	needToSave = () => {
-		/* return true if we need to save, false if we don't */
-		const { obsAnnotations, lastSavedObsAnnotations } = this.props;
-		return (
-			lastSavedObsAnnotations && obsAnnotations !== lastSavedObsAnnotations
-		);
-	};
+  needToSave = () => {
+    /* return true if we need to save, false if we don't */
+    const { obsAnnotations, lastSavedObsAnnotations } = this.props;
+    return (
+      lastSavedObsAnnotations && obsAnnotations !== lastSavedObsAnnotations
+    );
+  };
 
-	statusMessage() {
-		const { error } = this.props;
-		if (error) {
-			return `Autosave error: ${error}`;
-		}
-		return this.needToSave() ? "Unsaved" : "All saved";
-	}
+  statusMessage() {
+    const { error } = this.props;
+    if (error) {
+      return `Autosave error: ${error}`;
+    }
+    return this.needToSave() ? "Unsaved" : "All saved";
+  }
 
-	render() {
-		const { writableCategoriesEnabled } = this.props;
-		return writableCategoriesEnabled ? (
-			<div
-				id="autosave"
-				style={{
-					position: "fixed",
-					display: "inherit",
-					right: 0,
-					bottom: 0
-				}}
-			>
-				{this.statusMessage()}
-			</div>
-		) : null;
-	}
+  render() {
+    const { writableCategoriesEnabled } = this.props;
+    return writableCategoriesEnabled ? (
+      <div
+        id="autosave"
+        style={{
+          position: "fixed",
+          display: "inherit",
+          right: 5,
+          bottom: 5
+        }}
+      >
+        {this.statusMessage()}
+      </div>
+    ) : null;
+  }
 }
 
 export default Autosave;
