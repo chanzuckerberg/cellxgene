d4b52f086755be5964cf9515d27e5e5ff253ac7b
Colin Megill <colinmegill@gmail.com>
2019-10-14 13:17:04 -0700

    right sidebar file

---
+++ client/src/components/rightSidebar/index.js
@@ -0,0 +1,53 @@
+// jshint esversion: 6
+import React from "react";
+import { connect } from "react-redux";
+import Continuous from "../continuous/continuous";
+import GeneExpression from "../geneExpression";
+import * as globals from "../../globals";
+
+@connect(state => ({
+  responsive: state.responsive,
+  scatterplotXXaccessor: state.controls.scatterplotXXaccessor,
+  scatterplotYYaccessor: state.controls.scatterplotYYaccessor
+}))
+class RightSidebar extends React.Component {
+  render() {
+    const {
+      responsive,
+      scatterplotXXaccessor,
+      scatterplotYYaccessor
+    } = this.props;
+
+    /*
+    this magic number should be made less fragile,
+    if cellxgene logo or tabs change, this must as well
+    */
+    const logoRelatedPadding = 50;
+
+    return (
+      <div
+        style={{
+          position: "fixed",
+          right: 0,
+          backgroundColor: "white",
+          /* x y blur spread color */
+          borderLeft: `1px solid ${globals.lightGrey}`
+        }}
+      >
+        <div
+          style={{
+            height: responsive.height,
+            width: globals.leftSidebarWidth,
+            overflowY: "auto",
+            overflowX: "hidden"
+          }}
+        >
+          <GeneExpression />
+          <Continuous />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default RightSidebar;
