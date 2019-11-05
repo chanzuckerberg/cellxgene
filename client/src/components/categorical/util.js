2ac3a7338cddc703d7d62af1a676f3dabddad323
bkmartinjr <bruce@chanzuckerberg.com>
2019-11-04 18:12:34 -0800

    pin unassigned label to bottom of category list

--- client/src/components/categorical/util.js
+++ client/src/components/categorical/util.js
@@ -4,16 +4,29 @@
 // index is range array
 // return sorted index
 
-import isNumber from "is-number";
+/*
+Sort category values (labels) in the order we want for presentation.
 
-const sortedCategoryValues = values => {
+TL;DR: numeric sort for number-like strings, then strings in case-ignoring alpha
+order.  Except, when isUserAnno is true, pin globals.unassignedCategoryLabel 
+to the end.
+
+*/
+
+import isNumber from "is-number";
+import * as globals from "../../globals";
+
+const sortedCategoryValues = (isUserAnno, values) => {
   /* this sort could be memoized for perf */
 
   const strings = [];
   const ints = [];
+  const unassigned = [];
 
   values.forEach(v => {
-    if (isNumber(v[0])) {
+    if (isUserAnno && v[0] === globals.unassignedCategoryLabel) {
+      unassigned.push(v);
+    } else if (isNumber(v[0])) {
       ints.push(v);
     } else {
       strings.push(v);
@@ -28,7 +41,7 @@ const sortedCategoryValues = values => {
 
   ints.sort((a, b) => +a[0] - +b[0]);
 
-  return ints.concat(strings);
+  return ints.concat(strings, unassigned);
 };
 
 export default sortedCategoryValues;
