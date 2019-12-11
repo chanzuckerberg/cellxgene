80f7bb7aa4f45ca7572d6dfe7b74a7614fa8443f
Colin Megill <colinmegill@gmail.com>
2019-12-10 21:37:09 -0800

    render histos on open gene set

---
+++ client/src/components/geneExpression/gene.js
@@ -0,0 +1,31 @@
+// jshint esversion: 6
+/* rc slider https://www.npmjs.com/package/rc-slider */
+
+import React from "react";
+import _ from "lodash";
+import { connect } from "react-redux";
+import HistogramBrush from "../brushableHistogram";
+import { Button, ButtonGroup, Tooltip } from "@blueprintjs/core";
+import * as globals from "../../globals";
+import actions from "../../actions";
+
+@connect(state => {
+  return {};
+})
+class Gene extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    const { gene } = this.props;
+    return (
+      <div>
+        <p> {gene} </p>
+      </div>
+    );
+  }
+}
+
+export default Gene;
