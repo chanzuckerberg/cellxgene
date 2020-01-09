677433bbf50024e540ef0ef7f6894f0cc114bb01
Bruce Martin <bruce@chanzuckerberg.com>
2020-01-09 13:34:25 -0800

    Fix label sorting bugs (#1102)
    * move category label sort to utils
    
    * refactor cat label sort
    
    * fix category label sort and color assignment
    
    * convert whitespace from tabs to spaces

--- client/src/util/stateManager/schemaHelpers.js
+++ client/src/util/stateManager/schemaHelpers.js
@@ -4,93 +4,107 @@ Helpers for schema management
 import _ from "lodash";
 
 import fromEntries from "../fromEntries";
+import catLabelSort from "../catLabelSort";
 
 /*
 System wide schema assumptions:
-	- schema and data wil be consistent (eg, for user-created annotations)
-	- schema will be internally self-consistent (eg, index matches columns)
-	- world & universe schema are same - only data is subset
+  - schema and data wil be consistent (eg, for user-created annotations)
+  - schema will be internally self-consistent (eg, index matches columns)
+  - world & universe schema are same - only data is subset
 */
 
 export function indexEntireSchema(schema) {
-	/* Index schema for ease of use */
-	schema.annotations.obsByName = fromEntries(
-		schema.annotations.obs.columns.map(v => [v.name, v])
-	);
-	schema.annotations.varByName = fromEntries(
-		schema.annotations.var.columns.map(v => [v.name, v])
-	);
-	schema.layout.obsByName = fromEntries(
-		schema.layout.obs.map(v => [v.name, v])
-	);
-	schema.layout.varByName = fromEntries(
-		schema.layout.var.map(v => [v.name, v])
-	);
+  /* Index schema for ease of use */
+  schema.annotations.obsByName = fromEntries(
+    schema.annotations.obs.columns.map(v => [v.name, v])
+  );
+  schema.annotations.varByName = fromEntries(
+    schema.annotations.var.columns.map(v => [v.name, v])
+  );
+  schema.layout.obsByName = fromEntries(
+    schema.layout.obs.map(v => [v.name, v])
+  );
+  schema.layout.varByName = fromEntries(
+    schema.layout.var.map(v => [v.name, v])
+  );
 
-	return schema;
+  return schema;
+}
+
+export function sortAllCategorical(schema) {
+  /* UI relies on OBS annotation categories being in presentation sort order */
+  schema.annotations.obs.columns.forEach(c => {
+    if (c.categories) {
+      c.categories = catLabelSort(c.writable, c.categories);
+    }
+  });
 }
 
 function _copy(schema) {
-	/* redux copy conventions - WARNING, only for modifyign obs annotations */
-	return {
-		...schema,
-		annotations: {
-			...schema.annotations,
-			obs: _.cloneDeep(schema.annotations.obs)
-		}
-	};
+  /* redux copy conventions - WARNING, only for modifyign obs annotations */
+  return {
+    ...schema,
+    annotations: {
+      ...schema.annotations,
+      obs: _.cloneDeep(schema.annotations.obs)
+    }
+  };
 }
 
 function _reindex(schema) {
-	/* reindex obs annotations ONLY */
-	schema.annotations.obsByName = fromEntries(
-		schema.annotations.obs.columns.map(v => [v.name, v])
-	);
-	return schema;
+  /* reindex obs annotations ONLY */
+  schema.annotations.obsByName = fromEntries(
+    schema.annotations.obs.columns.map(v => [v.name, v])
+  );
+  return schema;
 }
 
 export function removeObsAnnoColumn(schema, name) {
-	const newSchema = _copy(schema);
-	newSchema.annotations.obs.columns = schema.annotations.obs.columns.filter(
-		v => v.name !== name
-	);
-	return _reindex(newSchema);
+  const newSchema = _copy(schema);
+  newSchema.annotations.obs.columns = schema.annotations.obs.columns.filter(
+    v => v.name !== name
+  );
+  return _reindex(newSchema);
 }
 
 export function addObsAnnoColumn(schema, name, defn) {
-	const newSchema = _copy(schema);
-	newSchema.annotations.obs.columns.push(defn);
-	return _reindex(newSchema);
+  const newSchema = _copy(schema);
+  newSchema.annotations.obs.columns.push(defn);
+  return _reindex(newSchema);
 }
 
 export function removeObsAnnoCategory(schema, name, category) {
-	/* remove a category from a categorical annotation */
-	const categories = schema.annotations.obsByName[name]?.categories;
-	if (!categories)
-		throw new Error("column does not exist or is not categorical");
+  /* remove a category from a categorical annotation */
+  const categories = schema.annotations.obsByName[name]?.categories;
+  if (!categories)
+    throw new Error("column does not exist or is not categorical");
 
-	const idx = categories.indexOf(category);
-	if (idx === -1) throw new Error("category does not exist");
+  const idx = categories.indexOf(category);
+  if (idx === -1) throw new Error("category does not exist");
 
-	const newSchema = _reindex(_copy(schema));
+  const newSchema = _reindex(_copy(schema));
 
-	/* remove category */
-	newSchema.annotations.obsByName[name].categories.splice(idx, 1);
-	return newSchema;
+  /* remove category.  Do not need to resort as this can't change presentation order */
+  newSchema.annotations.obsByName[name].categories.splice(idx, 1);
+  return newSchema;
 }
 
 export function addObsAnnoCategory(schema, name, category) {
-	/* add a category to a categorical annotation */
-	const categories = schema.annotations.obsByName[name]?.categories;
-	if (!categories)
-		throw new Error("column does not exist or is not categorical");
+  /* add a category to a categorical annotation */
+  const categories = schema.annotations.obsByName[name]?.categories;
+  if (!categories)
+    throw new Error("column does not exist or is not categorical");
 
-	const idx = categories.indexOf(category);
-	if (idx !== -1) throw new Error("category already exists");
+  const idx = categories.indexOf(category);
+  if (idx !== -1) throw new Error("category already exists");
 
-	const newSchema = _reindex(_copy(schema));
+  const newSchema = _reindex(_copy(schema));
 
-	/* remove category */
-	newSchema.annotations.obsByName[name].categories.push(category);
-	return newSchema;
+  /* add category, retaining presentation sort order */
+  const catAnno = newSchema.annotations.obsByName[name];
+  catAnno.categories = catLabelSort(catAnno.writable, [
+    ...catAnno.categories,
+    category
+  ]);
+  return newSchema;
 }
