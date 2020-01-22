2cc3f5be22ce332fe96c7e6792b35a4846163b17
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2020-01-22 11:09:36 -0800

    tooltip wording fix

--- client/src/components/menubar/index.js
+++ client/src/components/menubar/index.js
@@ -431,7 +431,7 @@ class MenuBar extends React.Component {
           </Tooltip>
         </ButtonGroup>
         <Tooltip
-          content="When a category is colored by, label categorical values on the graph"
+          content="When a category is colored by, show labels on the graph"
           position="bottom"
           disabled={graphInteractionMode === "zoom"}
         >
