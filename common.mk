f459bb2781199a3095f0ea901e71ba9671226d91
Matt Weiden <538456+mweiden@users.noreply.github.com>
2020-02-06 22:48:08 -0800

    Load env vars from environment.dev into Makefiles

--- common.mk
+++ common.mk
@@ -1,6 +1,14 @@
 SHELL=/bin/bash
 
-# CHECK ENVIRONMENT
-ifndef CXG_SERVER_PORT
-$(error Please run "source environment.<env>" in the root directory before running make commands)
-endif
+PROJECT_ROOT := $(shell git rev-parse --show-toplevel)
+
+# get_or_else_dev_env_default
+# - If a variable is defined, return its value
+# - Else return the default value from environment.dev
+define get_or_else_dev_env_default
+$(if $($(1)),$($(1)),$(shell sed -n 's/$(1)=\(.*\)/\1/p' < $(PROJECT_ROOT)/environment.dev))
+endef
+
+CXG_SERVER_PORT := $(call get_or_else_dev_env_default,CXG_SERVER_PORT)
+CXG_CLIENT_PORT := $(call get_or_else_dev_env_default,CXG_CLIENT_PORT)
+JEST_ENV := $(call get_or_else_dev_env_default,JEST_ENV)
