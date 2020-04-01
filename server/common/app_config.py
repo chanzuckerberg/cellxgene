cd3637610941e6dea1d5b54b1e6bf5c1f6ef937a
bkmartinjr <bruce@chanzuckerberg.com>
2020-04-01 14:58:18 -0700

    add config param check

--- server/common/app_config.py
+++ server/common/app_config.py
@@ -198,6 +198,7 @@ class AppConfig(object):
         self.__check_attr("server__open_browser", bool)
         self.__check_attr("server__force_https", bool)
         self.__check_attr("server__flask_secret_key", (type(None), str))
+        self.__check_attr("server__generate_cache_control_headers", bool)
 
         if self.server__port:
             if not is_port_available(self.server__host, self.server__port):
