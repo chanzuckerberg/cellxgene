56f143e287b5913b40671918416fa65a5429eee9
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2020-05-27 11:55:33 -0700

    remove font-family styling

--- client/src/components/graph/overlays/centroidLabels.js
+++ client/src/components/graph/overlays/centroidLabels.js
@@ -85,7 +85,6 @@ class CentroidLabels extends PureComponent {
             textAnchor="middle"
             data-label={label}
             style={{
-              fontFamily: "Roboto Condensed",
               fontSize,
               fontWeight,
               fill: "black",
