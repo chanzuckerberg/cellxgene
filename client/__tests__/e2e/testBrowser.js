344d7208aa66cfd72b27f43f8473ad07cfc21b00
bkmartinjr <bruce@chanzuckerberg.com>
2020-06-22 17:56:59 -0700

    re-fix the e2e test setup

--- client/__tests__/e2e/testBrowser.js
+++ client/__tests__/e2e/testBrowser.js
@@ -54,11 +54,11 @@ export default async function setupTestBrowser() {
     });
   }
   page.on("pageerror", (err) => {
-    console.log(`PAGE LOG: ${err.text()}`);
+    console.log(`PAGE LOG: ${err}`);
     throw new Error(`Console error: ${err}`);
   });
   page.on("error", (err) => {
-    console.log(`PAGE LOG: ${err.text()}`);
+    console.log(`PAGE LOG: ${err}`);
     throw new Error(`Console error: ${err}`);
   });
   const utils = puppeteerUtils(page);
