a6072b0a53d2dbb69d6bf4ceb9e6e9ac9b83d646
Matt Weiden <538456+mweiden@users.noreply.github.com>
2020-07-01 14:00:18 -0700

    Do not cache index.html

--- server/app/app.py
+++ server/app/app.py
@@ -56,8 +56,10 @@ def cache_control_always(**cache_kwargs):
     return _cache_control(True, **cache_kwargs)
 
 
+# tell the client not to cache the index.html page so that changes to the app work on redeployment
+# note that the bulk of the data needed by the client (datasets) will still be cached
 @webbp.route("/", methods=["GET"])
-@cache_control(public=True, max_age=ONE_WEEK)
+@cache_control_always(public=True, max_age=0, no_store=True, no_cache=True, must_revalidate=True)
 def dataset_index(url_dataroot=None, dataset=None):
     config = current_app.app_config
     if dataset is None:
