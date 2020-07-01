2b0c4692babbe7c9a76f144b4e2f40941b14800e
Timmy Huang <tihuan@users.noreply.github.com>
2020-06-30 21:27:38 -0700

    e2e-add-more-logs (#1598)

--- client/__tests__/e2e/screenshot_env.js
+++ client/__tests__/e2e/screenshot_env.js
@@ -6,19 +6,27 @@ const takeScreenshot = require("./takeScreenshot");
 
 class ScreenshotEnvironment extends PuppeteerEnvironment {
   async handleTestEvent(event, state) {
+    if (["test_start", "test_done"].includes(event.name)) {
+      console.log("------------------event name:\n", event.name);
+      console.log("~~~~ Current test errors\n", new Date(), event.test.errors);
+      console.log("~~~~ Current test\n", new Date(), event.test);
+    }
+
     if (event.name === "error") {
-      console.log("error", JSON.stringify(event));
+      console.log("error event:", JSON.stringify(event));
     }
 
     if (event.name === "test_fn_failure" || event.name === "hook_failure") {
+      console.log("------------------event name:\n", event.name);
+      console.log(">>>> Current state\n", new Date(), state);
+      console.log("===> Failure event\n", new Date(), event);
+
       // (thuang): We only want to take screenshot on the last try
       if (
         state.currentlyRunningTest.invocations <= ENV_DEFAULT.RETRY_ATTEMPTS
       ) {
         return;
       }
-
-      console.log("===> Failure event\n", new Date(), event);
 
       await takeScreenshot(state.currentlyRunningTest.name, this.global.page);
     }
