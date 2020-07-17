c2a307de6e845de6e9de382d862fd0dfc2c7998b
Brian McCandless <bmccandless@chanzuckerberg.com>
2020-07-16 22:23:17 -0700

    Fix error handling in plugin module

--- server/__init__.py
+++ server/__init__.py
@@ -1,6 +1,13 @@
 from server.common.utils import import_plugins
+import logging
+import sys
 
 __version__ = "0.15.0"
 display_version = "cellxgene v" + __version__
 
-import_plugins("server.plugins")
+try:
+    import_plugins("server.plugins")
+except Exception as e:
+    # Make sure to exit in this case, as the server may not be configured as expected.
+    logging.critical(f"Error in import_plugins: {str(e)}")
+    sys.exit(1)
