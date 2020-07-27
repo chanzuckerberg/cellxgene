c9e5e6337bb4a610b50501a9bffadfac94fe89ff
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2020-07-27 12:20:52 -0700

    propagate changes to prod

--- client/configuration/webpack/webpack.config.prod.js
+++ client/configuration/webpack/webpack.config.prod.js
@@ -91,21 +91,25 @@ module.exports = {
     }),
     new ObsoleteWebpackPlugin({
       template:
-        '<div class="outerContainer">' +
-        '<h1 class="purple"> Meta </h1>' +
-        '<div class="innerContainer">' +
-        '<div class="header"> Unsupported Browser </div>' +
-        '<div class="content"> Meta is currently supported on the following browsers' +
-        '<div class="supported-browsers purple">' +
-        '<div class="browser-type"> <img src="https://assets.beta.meta.org/images/browsers/chrome.png"/>' +
-        '<div class="version">Chrome > 60</div> </div>' +
-        '<div class="browser-type"> <img src="https://assets.beta.meta.org/images/browsers/safari.png"/>' +
-        '<div class="version">Safari >= 10.1</div> </div>' +
-        '<div class="browser-type"> <img src="https://assets.beta.meta.org/images/browsers/firefox.png"/>' +
-        '<div class="version">Firefox >= 60</div> </div>' +
-        '<div class="browser-type"><img src="https://assets.beta.meta.org/images/browsers/edge.png"/>' +
-        '<div class="version">Edge >= 15</div> </div>' +
-        "</div>",
+        "<script>" +
+        'var root = document.getElementById("root");' +
+        "root.remove();" +
+        'var portals = document.getElementsByClassName("bp3-portal");' +
+        "for(var i = 0; i < portals.length; i += 1) {" +
+        "portals[i].remove();" +
+        "}" +
+        "</script>" +
+        "<div>" +
+        '<div style="display: flex;flex-direction: column;width: 100vw;height: 100vh;text-align: center;justify-content: center;">' +
+        "<h1> cellxgene </h1>" +
+        "<div> Unsupported Browser </div>" +
+        '<div style="margin-top: 16px;"> cellxgene is currently supported on the following browsers' +
+        '<div style="display: flex;justify-content: space-around;margin: auto 40vw;margin-top: 16px">' +
+        '<div> <img src="https://assets.beta.meta.org/images/browsers/chrome.png" style="width: 80px;height: 80px;"><div>Chrome &gt; 60</div></div>' +
+        '<div> <img src="https://assets.beta.meta.org/images/browsers/safari.png" style="width: 80px;height: 80px;"><div>Safari &gt;= 10.1</div> </div>' +
+        '<div> <img src="https://assets.beta.meta.org/images/browsers/firefox.png" style="width: 80px;height: 80px;"><div>Firefox &gt;= 60</div> </div>' +
+        '<div><img src="https://assets.beta.meta.org/images/browsers/edge.png" style="width: 80px;height: 80px;"><div>Edge &gt;= 15</div> </div>' +
+        "</div></div><div></div></div>",
       promptOnNonTargetBrowser: true,
     }),
     new CleanWebpackPlugin({
