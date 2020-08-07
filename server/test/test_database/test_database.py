01fe3ae015a9155963fd22db68c8a1ec3ea32ebb
Madison Dunitz <dunitzm@gmail.com>
2020-08-07 15:58:39 -0500

    add function to retrieve latest annotation from db, db updates

--- server/test/test_database/test_database.py
+++ server/test/test_database/test_database.py
@@ -4,7 +4,7 @@ from server.db.db_utils import DbUtils
 from server.test.fixtures.database import TestDatabase
 
 
-class AppConfigTest(unittest.TestCase):
+class DatabaseTest(unittest.TestCase):
     db = DbUtils("postgresql://postgres:test_pw@localhost:5432")
 
     @classmethod
@@ -22,13 +22,39 @@ class AppConfigTest(unittest.TestCase):
         self.assertGreater(user_count, 10)
 
     def test_dataset_creation(self):
-        one_dataset = self.db.get(table=CellxGeneDataset, entity_id='test_dataset_id')
-        self.assertEqual(one_dataset.id, 'test_dataset_id')
+        one_dataset = self.db.query(table_args=[CellxGeneDataset],
+                                    filter_args=[CellxGeneDataset.name == 'test_dataset'])
+        self.assertEqual(one_dataset[0].name, 'test_dataset')
         dataset_count = self.db.session.query(CellxGeneDataset).count()
         self.assertGreater(dataset_count, 10)
 
     def test_annotation_creation(self):
-        one_annotation = self.db.get(table=Annotation, entity_id='test_annotation_id')
-        self.assertEqual(one_annotation.id, 'test_annotation_id')
+        one_annotation = self.db.query(table_args=[Annotation], filter_args=[Annotation.tiledb_uri == 'tiledb_uri'])[0]
+        self.assertEqual(one_annotation.tiledb_uri, 'tiledb_uri')
         annotation_count = self.db.session.query(Annotation).count()
         self.assertGreater(annotation_count, 10)
+
+    def test_get_most_recent_annotation_for_user_dataset(self):
+        dataset_id = str(self.db.query(table_args=[CellxGeneDataset],
+                                       filter_args=[CellxGeneDataset.name == 'test_dataset'])[0].id)
+
+        ## have to commit separately because created_at time written on the db server
+        self.db.session.add(Annotation(dataset_id=dataset_id, user_id='test_user_id', tiledb_uri='tiledb_uri_0'))
+        self.db.session.commit()
+
+        self.db.session.add(Annotation(dataset_id=dataset_id, user_id='test_user_id', tiledb_uri='tiledb_uri_1'))
+        self.db.session.commit()
+
+        self.db.session.add(Annotation(dataset_id=dataset_id, user_id='test_user_id', tiledb_uri='tiledb_uri_2'))
+        self.db.session.commit()
+
+        self.db.session.add(Annotation(dataset_id=dataset_id, user_id='test_user_id', tiledb_uri='tiledb_uri_3'))
+        self.db.session.commit()
+
+        self.db.session.add(Annotation(dataset_id=dataset_id, user_id='test_user_id', tiledb_uri='tiledb_uri_4'))
+        self.db.session.commit()
+
+        most_recent_annotation = self.db.query_for_most_recent(Annotation, [Annotation.dataset_id == dataset_id,
+                                                                            Annotation.user_id == 'test_user_id'])
+
+        self.assertEqual(most_recent_annotation.tiledb_uri, 'tiledb_uri_4')
