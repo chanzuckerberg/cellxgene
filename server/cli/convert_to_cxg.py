136b9334b98f91794a0a63dc616add6e0170bb64
maniarathi <arathi.mani@chanzuckerberg.com>
2020-08-09 00:28:25 -0700

    Lint

--- server/cli/convert_to_cxg.py
+++ server/cli/convert_to_cxg.py
@@ -1,7 +1,6 @@
 from os import path
 
 import click
-import numpy as np
 
 from server.converters.h5ad_data_file import H5ADDataFile
 
@@ -10,9 +9,9 @@ from server.converters.h5ad_data_file import H5ADDataFile
     name="convert",
     short_help="Converts an H5AD dataset to the CXG format.",
     help="Converts an H5AD dataset to the CXG format. The CXG format is a cellxgene-private data format "
-    "that has performance and access characteristics amenable to a multi-dataset, multi-user serving "
-    "environment. You will be able to launch the cellxgene using the `cellxgene launch` command as "
-    "usually with the generated CXG file.",
+         "that has performance and access characteristics amenable to a multi-dataset, multi-user serving "
+         "environment. You will be able to launch the cellxgene using the `cellxgene launch` command as "
+         "usually with the generated CXG file.",
 )
 @click.argument(
     "input-file",
@@ -24,13 +23,13 @@ from server.converters.h5ad_data_file import H5ADDataFile
     "-o",
     "--output-dir",
     help="Name of the output CXG directory. If not provided, will default to be the input filename with a "
-    "CXG extension.",
+         "CXG extension.",
 )
 @click.option(
     "-b",
     "--backed",
     help="When true, loads the H5AD in file backed mode. This will cause the conversion to be slower, "
-    "but will use less memory.",
+         "but will use less memory.",
     default=False,
     show_default=True,
     is_flag=True,
@@ -39,20 +38,20 @@ from server.converters.h5ad_data_file import H5ADDataFile
     "-t",
     "--title",
     help="Human readable dataset title that will be included as metadata about the CXG file. If omitted, "
-    "the dataset title will be the filename.",
+         "the dataset title will be the filename.",
 )
 @click.option(
     "-a",
     "--about",
     help="A fully qualified URL that provides more information about the dataset and will be included as "
-    "metadata about the CXG file.",
+         "metadata about the CXG file.",
 )
 @click.option(
     "-s",
     "--sparse-threshold",
     help="If the dataset's percent of non-zero values falls belows the specified threshold, then the X "
-    "array of the dataset will be sparse. Since the default value is 0.0, the default will be to "
-    "convert to dense array.",
+         "array of the dataset will be sparse. Since the default value is 0.0, the default will be to "
+         "convert to dense array.",
     default=0.0,
     show_default=True,
 )
@@ -84,17 +83,17 @@ from server.converters.h5ad_data_file import H5ADDataFile
 @click.option("-v", "--verbose", count=True)
 @click.help_option("--help", "-h", help="Show this message and exit.")
 def convert_to_cxg(
-    input_file,
-    output_directory,
-    backed,
-    title,
-    about,
-    sparse_threshold,
-    obs_names,
-    var_names,
-    disable_custom_colors,
-    disable_corpora_schema,
-    should_overwrite,
+        input_file,
+        output_directory,
+        backed,
+        title,
+        about,
+        sparse_threshold,
+        obs_names,
+        var_names,
+        disable_custom_colors,
+        disable_corpora_schema,
+        should_overwrite,
 ):
     """
     Convert a dataset file into CXG.
@@ -106,8 +105,6 @@ def convert_to_cxg(
     cxg_output_container = get_output_directory(input_file, output_directory, should_overwrite)
 
     h5ad_data_file.to_cxg(cxg_output_container, not disable_custom_colors, sparse_threshold)
-
-
 
 
 def get_output_directory(input_filename, output_directory, should_overwrite):
