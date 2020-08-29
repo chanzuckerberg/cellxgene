b8b34289635e867c2e85f2274fb06e60ca0db6db
maniarathi <arathi.mani@chanzuckerberg.com>
2020-08-29 10:54:12 -0700

    Bump version: 0.16.0 → 0.16.1

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("server/requirements-prepare.txt") as fh:
 
 setup(
     name="cellxgene",
-    version="0.16.0",
+    version="0.16.1",
     packages=find_packages(),
     url="https://github.com/chanzuckerberg/cellxgene",
     license="MIT",
