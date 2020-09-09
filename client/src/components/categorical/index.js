7557be09ea98930fe25a5fb2b6167d5ed8acfeeb
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2020-09-09 10:56:10 -0700

    remove categorical move note

--- client/src/components/categorical/index.js
+++ client/src/components/categorical/index.js
@@ -121,21 +121,6 @@ class Categories extends React.Component {
     }
   };
 
-  handleMouseOver = () => {
-    this.dispatchSingletonHover("ON");
-  };
-
-  handleMouseOut = () => {
-    this.dispatchSingletonHover("OFF");
-  };
-
-  dispatchSingletonHover = (state) => {
-    const { dispatch } = this.props;
-
-    if (state === "ON") dispatch({ type: "singleton hover on" });
-    if (state === "OFF") dispatch({ type: "singleton hover off" });
-  };
-
   render() {
     const {
       createAnnoModeActive,
@@ -210,14 +195,6 @@ class Categories extends React.Component {
             />
           ) : null
         )}
-        <span
-          onMouseOver={this.handleMouseOver}
-          onMouseOut={this.handleMouseOut}
-          onFocus={this.handleMouseOver}
-          onBlur={this.handleMouseOut}
-        >
-          single value categories have moved to dataset overview
-        </span>
         {/* WRITEABLE FIELDS */}
         {allCategoryNames.map((catName) =>
           schema.annotations.obsByName[catName].writable ? (
