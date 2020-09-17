023b1261eb1c3a78a5eb7ff6daca2fc0273d1314
Marcus Kinsella <mkinsella@chanzuckerberg.com>
2020-09-17 12:29:43 -0700

    Hook up remix function to CLI

--- server/cli/schema.py
+++ server/cli/schema.py
@@ -1,10 +1,12 @@
 import click
 
+from server.converters.schema import remix
+
 
 @click.group(
     name="schema",
     subcommand_metavar="COMMAND <args>",
-    short_help="Apply and validate teh cellxgene data integration schema to an h5ad file.",
+    short_help="Apply and validate the cellxgene data integration schema to an h5ad file.",
     context_settings=dict(max_content_width=85, help_option_names=["-h", "--help"]),
 )
 def schema_cli():
@@ -17,19 +19,28 @@ def schema_cli():
     help="Using a yaml file that describes schema values to insert or convert and in input "
     "h5ad file, apply the schema changes and create a new, conforming h5ad.",
 )
-@click.argument(
+@click.option(
     "--source-h5ad",
+    help="Input h5ad file.",
     nargs=1,
+    required=True,
     type=click.Path(exists=True, dir_okay=False),
 )
-@click.argument(
+@click.option(
     "--remix-config",
+    help="Config yaml with information on how to apply the schema.",
     nargs=1,
+    required=True,
     type=click.Path(exists=True, dir_okay=False),
 )
-@click.argument("--output_filename", nargs=1)
+@click.option(
+    "--output_filename",
+    help="Filename for the new, schema-conforming h5ad file.",
+    required=True,
+    nargs=1
+)
 def schema_apply(source_h5ad, remix_config, output_filename):
-    pass
+    remix.apply_schema(source_h5ad, remix_config, output_filename)
 
 
 @click.command(
