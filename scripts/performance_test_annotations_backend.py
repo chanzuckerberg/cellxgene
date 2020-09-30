6d590f34876ddd05e572a3ff7f8691062f594f55
Madison Dunitz <dunitzm@gmail.com>
2020-09-29 19:29:29 -0500

    make testing plan

---
+++ scripts/performance_test_annotations_backend.py
@@ -0,0 +1,10 @@
+
+## Find data sets with 10k, 100k, 1 millions cells
+## Function to Generate X number of categories (with full array)
+## Function to generate X different labels of Y length for each cell in the dataset
+## Use locust to hit endpoint with different numbers of
+# - [ ]users how many??  -- does it need to be with different users?
+#  - [ ]how to do auth? -- need to sign in to get token? -- store tokens and use randomly
+
+## create scheduled github action
+## clean up db/s3?
