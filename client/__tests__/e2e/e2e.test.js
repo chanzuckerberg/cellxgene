0e496414511cc2ddb96da6da4757b9423dd0b4a9
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2020-10-08 22:07:49 -0600

    add and check system arg to state auth type

--- client/__tests__/e2e/e2e.test.js
+++ client/__tests__/e2e/e2e.test.js
@@ -522,7 +522,10 @@ test("lasso moves after pan", async () => {
   expect(panCount).toBe(initialCount);
 });
 
-describe("auth buttons", () => {
+const describeIfCalledByMakeFileTarget =
+  process.env.CXG_AUTH_TYPE === "test" ? describe : describe.skip;
+
+describeIfCalledByMakeFileTarget("auth buttons", () => {
   test("login then logout", async () => {
     await goToPage(appUrlBase);
     await clickOnUntil("log-in", async () => {
