1d0d5ac0a190b9a13064f28adb462139b59c1497
maniarathi <arathi.mani@chanzuckerberg.com>
2020-10-12 09:37:49 -0700

    Remove async code

--- client/src/components/infoDrawer/infoDrawer.js
+++ client/src/components/infoDrawer/infoDrawer.js
@@ -1,7 +1,6 @@
 import React, { PureComponent } from "react";
-import { connect, shallowEqual } from "react-redux";
+import { connect } from "react-redux";
 import { Drawer } from "@blueprintjs/core";
-import Async from "react-async";
 
 import InfoFormat from "./infoFormat";
 import { selectableCategoryNames } from "../../util/stateManager/controlsHelpers";
@@ -17,27 +16,6 @@ import { selectableCategoryNames } from "../../util/stateManager/controlsHelpers
   };
 })
 class InfoDrawer extends PureComponent {
-  static watchAsync(props, prevProps) {
-    return !shallowEqual(props.watchProps, prevProps.watchProps);
-  }
-
-  fetchAsyncProps = async (props) => {
-    const { schema } = props.watchProps;
-
-    const allCategoryNames = selectableCategoryNames(schema).sort();
-    const singleValueCategories = new Map();
-
-    allCategoryNames.forEach((catName) => {
-      const isUserAnno = schema?.annotations?.obsByName[catName]?.writable;
-      const colSchema = schema.annotations.obsByName[catName];
-      if (!isUserAnno && colSchema.categories?.length === 1) {
-        singleValueCategories.set(catName, colSchema.categories[0]);
-      }
-    });
-
-    return { singleValueCategories };
-  };
-
   handleClose = () => {
     const { dispatch } = this.props;
 
@@ -54,45 +32,31 @@ class InfoDrawer extends PureComponent {
       dataPortalProps,
     } = this.props;
 
+    const allCategoryNames = selectableCategoryNames(schema).sort();
+    const singleValueCategories = new Map();
+
+    allCategoryNames.forEach((catName) => {
+      const isUserAnno = schema?.annotations?.obsByName[catName]?.writable;
+      const colSchema = schema.annotations.obsByName[catName];
+      if (!isUserAnno && colSchema.categories?.length === 1) {
+        singleValueCategories.set(catName, colSchema.categories[0]);
+      }
+    });
+
     return (
       <Drawer
         title="Dataset Overview"
         onClose={this.handleClose}
         {...{ isOpen, position }}
       >
-        <Async
-          watchFn={InfoDrawer.watchAsync}
-          promiseFn={this.fetchAsyncProps}
-          watchProps={{ schema }}
-        >
-          <Async.Pending>
-            <InfoFormat
-              skeleton
-              {...{ datasetTitle, aboutURL, dataPortalProps }}
-            />
-          </Async.Pending>
-          <Async.Rejected>
-            {(error) => {
-              console.error(error);
-              return <span>Failed to load info</span>;
-            }}
-          </Async.Rejected>
-          <Async.Fulfilled>
-            {(asyncProps) => {
-              const { singleValueCategories } = asyncProps;
-              return (
-                <InfoFormat
-                  {...{
-                    datasetTitle,
-                    aboutURL,
-                    singleValueCategories,
-                    dataPortalProps,
-                  }}
-                />
-              );
-            }}
-          </Async.Fulfilled>
-        </Async>
+        <InfoFormat
+          {...{
+            datasetTitle,
+            aboutURL,
+            singleValueCategories,
+            dataPortalProps,
+          }}
+        />
       </Drawer>
     );
   }
