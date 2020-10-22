158a4c7d43a30f187f2c48705c15ce9d772ee2ae
maniarathi <arathi.mani@chanzuckerberg.com>
2020-10-22 14:18:03 -0700

    Bump version: 0.16.5-rc.2 → 0.16.5

--- server/__init__.py
+++ server/__init__.py
@@ -2,7 +2,7 @@ import logging
 import sys
 from server.common.utils.utils import import_plugins
 
-__version__ = "0.16.5-rc.2"
+__version__ = "0.16.5"
 display_version = "cellxgene v" + __version__
 
 try:
