62464c0733f5cef4c2eb6c1c507b848670327406
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2020-12-14 12:59:28 -0800

    don't timeout first call

--- client/src/util/exponentialBackoffFetch.js
+++ client/src/util/exponentialBackoffFetch.js
@@ -28,31 +28,28 @@ export default function createBackoffFetch(retryTimes = 3) {
       delayFetch();
 
       function delayFetch() {
-        timeoutId = setTimeout(async () => {
-          console.log("args:", fetchArgs[2], "attempt:", retryCount);
-          try {
-            const response = await mockFetch(...fetchArgs);
-
-            if (!response.ok) {
-              const { status, statusText } = response;
-
-              throw Error(`${status}: ${statusText}`);
-            }
-            console.log("success", fetchArgs[2]);
-
-            resolve(response);
-          } catch (error) {
-            console.log("error", fetchArgs[2]);
-            if (retryCount === retryTimes) {
-              reject(error);
-            }
-
-            retryCount += 1;
-
-            delayFetch();
-          }
+        if (retryCount === 0) {
+          fetchAndHandle();
           // delay amount (3.5, 17.5, 87.5, 437.5 seconds)
-        }, retryCount * 700 * 5 ** retryCount);
+        } else timeoutId = setTimeout(fetchAndHandle, 700 * 5 ** retryCount);
+      }
+      async function fetchAndHandle() {
+        try {
+          promise.response = await mockFetch(...fetchArgs);
+
+          if (promise.response.ok || retryCount === retryTimes) {
+            resolve(promise.response);
+          }
+          console.log("success", fetchArgs[2]);
+        } catch (error) {
+          console.log("error", fetchArgs[2]);
+          if (retryCount === retryTimes) {
+            reject(error);
+          }
+        }
+        retryCount += 1;
+
+        delayFetch();
       }
     }
   }
@@ -64,13 +61,10 @@ export default function createBackoffFetch(retryTimes = 3) {
 function mockFetch() {
   return new Promise((resolve, reject) => {
     const rand = Math.floor(Math.random() * 10) + 1;
-    const response = {};
-    setTimeout(() => {
-      if (rand > 5) {
-        if (rand % 2) reject(new Error("fetch error"));
-        response.ok = false;
-      } else response.ok = true;
-      resolve(response);
-    }, rand * 500);
+    const response = { ok: false };
+    if (rand > 5) {
+      if (rand % 1 === 1) reject(new Error("fetch error"));
+    } else response.ok = true;
+    resolve(response);
   });
 }
