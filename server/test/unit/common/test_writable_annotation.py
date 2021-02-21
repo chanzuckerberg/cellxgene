eb9243aab2bd6d018b44206be2de9803b3a2e730
Arathi Mani <arathi.mani@chanzuckerberg.com>
2021-02-21 13:03:12 -0800

    Swap H5AD test for CXG test.

--- server/test/unit/common/test_writable_annotation.py
+++ server/test/unit/common/test_writable_annotation.py
@@ -161,7 +161,7 @@ class WritableTileDBStoredAnnotationTest(unittest.TestCase):
 
 class WritableAnnotationTest(unittest.TestCase):
     def setUp(self):
-        self.data, self.tmp_dir, self.annotations = data_with_tmp_annotations(MatrixDataType.H5AD)
+        self.data, self.tmp_dir, self.annotations = data_with_tmp_annotations(MatrixDataType.CXG)
         self.data.dataset_config.user_annotations = self.annotations
 
     def tearDown(self):
