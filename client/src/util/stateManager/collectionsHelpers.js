a3d936bfe2d3cf6bf980ab0d205d48059d90b3a4
Mim Hastie <mim@clevercanary.com>
2021-05-11 08:56:36 -0700

    Added dataset selector. (#176) (#2195)

--- client/src/util/stateManager/collectionsHelpers.js
+++ client/src/util/stateManager/collectionsHelpers.js
@@ -2,10 +2,26 @@
 Helper functions for querying and binding Portal collections.
 */
 
+const LOCATION_ORIGIN = window.location.origin;
+
+export function createCollectionsViewModel(collections) {
+  /*
+  update origin and data root of dataset deployment URLs to format expected for current environment, sort datasets
+  by created date.
+  TODO(cc) temp while we read collections from static JSON file
+   */
+  return collections.map((collection) => {
+    return {
+      ...collection,
+      datasets: createDatasetsViewModel(collection.datasets),
+    };
+  });
+}
+
 export function lookupDatasetIdByPath(path, collections) {
   /*
-  determine current dataset ID by mapping from URL path parameters to dataset values in specified set of collections 
-  TODO(cc) temp workaround until dataset ID is added to response
+  determine current dataset ID by mapping from URL path parameters to dataset values in specified set of collections .
+  TODO(cc) temp until dataset ID is added to response
    */
   const deploymentId = bindDeploymentId(path);
   return findDatasetIdByDeploymentId(deploymentId, collections);
@@ -13,7 +29,7 @@ export function lookupDatasetIdByPath(path, collections) {
 
 export function keyCollectionsByDatasetId(collections) {
   /*
-  create convenience map facilitating select of collection for a given dataset. 
+  create convenience map facilitating select of collection for a given dataset.
   */
   return collections.reduce((collectionsByDatasetId, collection) => {
     (collection.datasets ?? []).forEach((dataset) => {
@@ -24,20 +40,30 @@ export function keyCollectionsByDatasetId(collections) {
   }, new Map());
 }
 
-function bindDeploymentId(path) {
+function bindDeploymentId(pathOrUrl) {
   /*
-  read name of cxg from path. given "protocol://hostname/x/any-alpha-numeric.cxg/", match on "/any-alpha-numeric.cxg/".
+  read name of cxg from path. given "protocol://hostname/x/any-alpha-numeric.cxg/", match on "/any-alpha-numeric.cxg/",
+  group on "any-alpha-numeric.cxg".
    */
-  const matches = path.match(/\/([a-z0-9_.-]*\.cxg)\//i);
+  const matches = pathOrUrl.match(/\/([a-z0-9_.-]*\.cxg)\//i);
   if (!matches || matches.length < 2) {
     // Expecting at least match and one capturing group
-    throw new Error("Unable to bind deployment ID from URL");
+    throw new Error(`Unable to bind deployment ID from "${pathOrUrl}"`);
   }
   return matches[1];
 }
 
+function switchUrlOriginToCurrent(url) {
+  /*
+  swap out origin in the given URL to the current origin.
+  TODO(cc) generalize data root per environment, use /\/[a-z0-9_.-]*\/[a-z0-9_.-]*\.cxg\//i to match data root and deployment ID.
+   */
+  const deploymentId = bindDeploymentId(url);
+  return `${LOCATION_ORIGIN}/d/${deploymentId}/`;
+}
+
 function findDatasetIdByDeploymentId(deploymentId, collections) {
-  /**
+  /*
    return the ID of the dataset that has a deployment URL containing the specified deployment ID. 
    */
   let selectedDatasetId;
@@ -58,4 +84,20 @@ function findDatasetIdByDeploymentId(deploymentId, collections) {
     );
   }
   return selectedDatasetId;
+}
+
+function createDatasetsViewModel(datasets) {
+  /*
+   update origin of deployment URL of each dataset to be the current origin. sort by created date descending.
+   */
+  const viewModels = datasets.map((dataset) => {
+    return {
+      ...dataset,
+      url: switchUrlOriginToCurrent(dataset.url),
+    };
+  });
+  viewModels.sort((vm0, vm1) => {
+    return vm0.createdAt - vm1.createdAt;
+  });
+  return viewModels;
 }
