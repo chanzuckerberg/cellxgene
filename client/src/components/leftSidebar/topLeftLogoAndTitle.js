d0a7a9039458f6086250f4dab69f99557e01eec2
MillenniumFalconMechanic <mim@clevercanary.com>
2021-05-13 10:09:32 -0700

    Updated drawer toggle location. (#183)

--- client/src/components/leftSidebar/topLeftLogoAndTitle.js
+++ client/src/components/leftSidebar/topLeftLogoAndTitle.js
@@ -1,44 +1,26 @@
 import React from "react";
 import { connect } from "react-redux";
-import { Button } from "@blueprintjs/core";
 
 import * as globals from "../../globals";
 import Logo from "../framework/logo";
-import Truncate from "../util/truncate";
-import InfoDrawer from "../infoDrawer/infoDrawer";
 import InformationMenu from "./infoMenu";
 
-const DATASET_TITLE_FONT_SIZE = 14;
-
 @connect((state) => {
-  const { corpora_props: corporaProps } = state.config;
-  const correctVersion =
-    ["1.0.0", "1.1.0"].indexOf(corporaProps?.version?.corpora_schema_version) >
-    -1;
   return {
-    datasetTitle: state.config?.displayNames?.dataset ?? "",
     libraryVersions: state.config?.library_versions,
     aboutLink: state.config?.links?.["about-dataset"],
     tosURL: state.config?.parameters?.about_legal_tos,
     privacyURL: state.config?.parameters?.about_legal_privacy,
-    title: correctVersion ? corporaProps?.title : undefined,
   };
 })
 class LeftSideBar extends React.Component {
-  handleClick = () => {
-    const { dispatch } = this.props;
-    dispatch({ type: "toggle dataset drawer" });
-  };
-
   render() {
     const {
-      datasetTitle,
       libraryVersions,
       aboutLink,
       privacyURL,
       tosURL,
       dispatch,
-      title,
     } = this.props;
 
     return (
@@ -82,22 +64,6 @@ class LeftSideBar extends React.Component {
           </span>
         </div>
         <div style={{ marginRight: 5, height: "100%" }}>
-          <Button
-            minimal
-            style={{
-              fontSize: DATASET_TITLE_FONT_SIZE,
-              position: "relative",
-              top: -1,
-            }}
-            onClick={this.handleClick}
-          >
-            <Truncate>
-              <span style={{ maxWidth: 155 }} data-testid="header">
-                {title ?? datasetTitle}
-              </span>
-            </Truncate>
-          </Button>
-          <InfoDrawer />
           <InformationMenu
             {...{
               libraryVersions,
