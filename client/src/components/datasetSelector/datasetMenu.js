91e8adea70d15ff7eb2409f17526d0749b212b29
Mim Hastie <mim@clevercanary.com>
2021-05-20 11:33:53 -0700

    Refine dataset selector UI to match mocks. (#177) (#2215)

--- client/src/components/datasetSelector/datasetMenu.js
+++ client/src/components/datasetSelector/datasetMenu.js
@@ -1,12 +1,9 @@
 /* core dependencies */
-import {
-  Breadcrumb,
-  Menu,
-  MenuItem,
-  Popover,
-  Position,
-} from "@blueprintjs/core";
+import { Colors, Menu, MenuItem, Popover, Position } from "@blueprintjs/core";
 import React from "react";
+
+/* styles */
+import styles from "./datasetSelector.css";
 
 const buildDatasetMenuItems = (datasets) => {
   /*
@@ -19,19 +16,36 @@ const buildDatasetMenuItems = (datasets) => {
 /*
  dataset menu, toggled from dataset name in app-level breadcrumbs
  */
-const DatasetMenu = React.memo(({ datasets, selectedDatasetName }) => {
+const DatasetMenu = React.memo(({ children, datasets }) => {
+  const menuScrollable = datasets.length > 9; /* scrollable at 10 datasets */
   return (
     <Popover
       content={
-        <Menu style={{ maxHeight: 305, overflow: "scroll" }}>
+        <Menu
+          className={menuScrollable ? styles.datasetMenuScrollable : null}
+          style={{
+            color: Colors.BLACK,
+            maxHeight:
+              "290px" /* show 9.5 datasets at 30px height each, plus top padding of 5px */,
+            maxWidth: "680px" /* TODO(cc) revisit max-width versus width */,
+            overflow: "auto",
+            paddingRight: menuScrollable
+              ? 0 /* scrollbar adds padding to menu  */ /* TODO(cc) address ff requiring additional padding */
+              : null,
+          }}
+        >
           {buildDatasetMenuItems(datasets)}
         </Menu>
       }
+      hasBackdrop
+      minimal
+      modifiers={{ offset: { offset: "0, 10" } }}
+      popoverClassName={styles.datasetPopover}
       position={Position.BOTTOM_LEFT}
+      targetClassName={styles.datasetPopoverTarget}
     >
-      <Breadcrumb>{selectedDatasetName}</Breadcrumb>
+      {children}
     </Popover>
   );
 });
-
 export default DatasetMenu;
