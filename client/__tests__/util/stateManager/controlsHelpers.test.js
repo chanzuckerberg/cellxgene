bc9ed01fd25a00f8ccaab5960c8de9b591d28cba
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-05-25 10:25:29 -0700

    restore controlHelpers test with todo

---
+++ client/__tests__/util/stateManager/controlsHelpers.test.js
@@ -0,0 +1,8 @@
+/*
+test controls helpers
+*/
+
+describe("controls helpers", () => {
+  // TODO #2227 test: improve test coverage on control helper functions
+  // (`topNCategories()`, `isSelectableCategoryName()`, `selectableCategoryNames()`, `createCategorySummaryFromDfCol()`, `createCategoricalSelection()`, )
+});
