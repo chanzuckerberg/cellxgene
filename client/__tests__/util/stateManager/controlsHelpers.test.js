7ed53c0f5b5db9efa89890875441258aa49679a4
Severiano Badajoz <sbadajoz@chanzuckerberg.com>
2021-05-26 12:41:16 -0700

    fix: add placeholder test so suite doesn't fail (#2233)
    * comment out test suite
    
    * actually just put in placeholdertest

--- client/__tests__/util/stateManager/controlsHelpers.test.js
+++ client/__tests__/util/stateManager/controlsHelpers.test.js
@@ -1,8 +1,9 @@
 /*
 test controls helpers
 */
+// TODO #2227 test: improve test coverage on control helper functions
+// (`topNCategories()`, `isSelectableCategoryName()`, `selectableCategoryNames()`, `createCategorySummaryFromDfCol()`, `createCategoricalSelection()`, )
 
 describe("controls helpers", () => {
-  // TODO #2227 test: improve test coverage on control helper functions
-  // (`topNCategories()`, `isSelectableCategoryName()`, `selectableCategoryNames()`, `createCategorySummaryFromDfCol()`, `createCategoricalSelection()`, )
+  test("placeholder", () => {});
 });
