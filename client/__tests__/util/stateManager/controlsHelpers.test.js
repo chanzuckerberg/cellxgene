4d03b5a025b71f53f4c52535a8a036408071bb5b
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-05-26 11:14:45 -0700

    comment out test suite

--- client/__tests__/util/stateManager/controlsHelpers.test.js
+++ client/__tests__/util/stateManager/controlsHelpers.test.js
@@ -1,8 +1,8 @@
 /*
 test controls helpers
 */
+// TODO #2227 test: improve test coverage on control helper functions
+// (`topNCategories()`, `isSelectableCategoryName()`, `selectableCategoryNames()`, `createCategorySummaryFromDfCol()`, `createCategoricalSelection()`, )
 
-describe("controls helpers", () => {
-  // TODO #2227 test: improve test coverage on control helper functions
-  // (`topNCategories()`, `isSelectableCategoryName()`, `selectableCategoryNames()`, `createCategorySummaryFromDfCol()`, `createCategoricalSelection()`, )
-});
+// describe("controls helpers", () => {
+// });
