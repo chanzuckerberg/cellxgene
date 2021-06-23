ada14391db6a4103e29c99e9925c50c16eb3f2eb
atarashansky <alexander.tarashansky@atarashansky-mba.local>
2021-06-22 22:25:15 -0700

    fixed bug, added differential as a dependency to useMemo

--- client/src/components/hotkeys/index.js
+++ client/src/components/hotkeys/index.js
@@ -44,7 +44,7 @@ export const HotkeyTest = (props) => {
         onKeyDown: () => dispatch(actions.selectAll()),
       },
     ],
-    []
+    [differential]
   );
   const { handleKeyDown, handleKeyUp } = useHotkeys(hotkeys);
 
