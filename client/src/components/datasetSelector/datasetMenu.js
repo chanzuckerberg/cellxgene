0e89d6fa201ba41b99ba94b113342f75941dce9b
Fran McDade <frano-m@users.noreply.github.com>
2021-06-30 03:50:50 +1000

    Document and clarify Blueprint override approaches and support overhead. Refactor styles. (#195) (#2260)

--- client/src/components/datasetSelector/datasetMenu.js
+++ client/src/components/datasetSelector/datasetMenu.js
@@ -1,5 +1,5 @@
 /* core dependencies */
-import { Colors, Menu, MenuItem, Popover, Position } from "@blueprintjs/core";
+import { Menu, MenuItem, Popover, Position } from "@blueprintjs/core";
 import React from "react";
 
 /* styles */
@@ -23,22 +23,16 @@ const buildDatasetMenuItems = (datasets) => {
  dataset menu, toggled from dataset name in app-level breadcrumbs
  */
 const DatasetMenu = React.memo(({ children, datasets }) => {
-  const menuScrollable = datasets.length > 9; /* scrollable at 10 datasets */
   return (
     <Popover
       boundary="viewport"
       content={
         <Menu
-          className={menuScrollable ? styles.datasetMenuScrollable : null}
           style={{
-            color: Colors.BLACK,
             maxHeight:
               "290px" /* show 9.5 datasets at 30px height each, plus top padding of 5px */,
             maxWidth: "680px" /* TODO(cc) revisit max-width versus width */,
             overflow: "auto",
-            paddingRight: menuScrollable
-              ? 0 /* scrollbar adds padding to menu  */ /* TODO(cc) address ff requiring additional padding */
-              : null,
           }}
         >
           {buildDatasetMenuItems(datasets)}
