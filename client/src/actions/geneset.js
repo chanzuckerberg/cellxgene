f32a850defc774b2c97669558997d1f5947c7f34
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-06-29 15:57:58 -0700

    fix: reset gene set colorScale on gene set deletion

--- client/src/actions/geneset.js
+++ client/src/actions/geneset.js
@@ -16,7 +16,7 @@ The behavior manifest in these action creators:
     Add a gene to a gene set, will:
       * drop index & clear selection state on the gene set summary
       * will NOT touch the selection state for the gene
-  
+
 Note that crossfilter indices are lazy created, as needed.
 */
 
@@ -26,6 +26,11 @@ export const genesetDelete = (genesetName) => (dispatch, getState) => {
   const gs = genesets?.genesets?.get(genesetName) ?? {};
   const geneSymbols = Array.from(gs.genes.keys());
   const obsCrossfilter = dropGeneset(dispatch, state, genesetName, geneSymbols);
+  if (genesetName === state.colors.colorAccessor) {
+    dispatch({
+      type: "reset colorscale",
+    });
+  }
   dispatch({
     type: "geneset: delete",
     genesetName,
