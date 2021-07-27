615b21b1f262dd674c62a10c5e786a12134bdd66
bkmartinjr <bruce@chanzuckerberg.com>
2021-07-27 07:42:52 -0700

    handle non-finites explicitly

--- backend/common/compute/estimate_distribution.py
+++ backend/common/compute/estimate_distribution.py
@@ -5,27 +5,51 @@ from scipy import sparse
 from backend.common.constants import XApproxDistribution
 
 
+def dtype_limits(arr: np.ndarray):
+    if arr.dtype.kind == "f":
+        finf = np.finfo(arr.dtype)
+        return (finf.min, finf.max)
+    if arr.dtype.kind in ["u", "i"]:
+        iinf = np.iinfo(arr.dtype)
+        return (iinf.min, iinf.max)
+    # error
+    return (0, 0)
+
+
 @numba.njit(fastmath=True, error_model="numpy", nogil=True)
-def min_max(arr):
+def min_max(arr: np.ndarray, min_limit, max_limit):
     """Return (min, max) values for the ndarray."""
     n = arr.size
     odd = n % 2
-    if not odd:
+    if odd:
         n -= 1
-    max_val = min_val = arr[0]
-    i = 1
+
+    max_val, min_val = min_limit, max_limit
+
+    i = 0
     while i < n:
         x = arr[i]
         y = arr[i + 1]
+
+        # ignore non-finites
+        x = x if np.isfinite(x) else min_val
+        y = y if np.isfinite(y) else min_val
+
         if x > y:
             x, y = y, x
         min_val = min(x, min_val)
         max_val = max(y, max_val)
         i += 2
-    if not odd:
+
+    if odd:
         x = arr[n]
+
+        # ignore non-finites
+        x = x if np.isfinite(x) else min_val
+
         min_val = min(x, min_val)
         max_val = max(x, max_val)
+
     return min_val, max_val
 
 
@@ -38,6 +62,10 @@ def estimate_approximate_distribution(X) -> XApproxDistribution:
     any (max-min) range in excess of 24 is implies tens of millions of
     observations of a single feature and so is extremely unlikely.
     """
+    if X.size == 0:
+        # default
+        return XApproxDistribution.NORMAL
+
     if sparse.isspmatrix_csc(X) or sparse.isspmatrix_csr(X):
         Xdata = X.data
     elif type(X) is np.ndarray:
@@ -47,16 +75,21 @@ def estimate_approximate_distribution(X) -> XApproxDistribution:
     else:
         raise TypeError(f"Unsupported matrix type: {str(type(X))}")
 
+    min_limit, max_limit = dtype_limits(Xdata)
+
     CHUNKSIZE = 1 << 24
     if Xdata.size > CHUNKSIZE:
         min_val = max_val = Xdata[0]
         with concurrent.futures.ThreadPoolExecutor() as tp:
-            for (_min, _max) in tp.map(min_max, [Xdata[i : i + CHUNKSIZE] for i in range(0, Xdata.size, CHUNKSIZE)]):
+            for (_min, _max) in tp.map(
+                lambda p: min_max(*p),
+                [[Xdata[i : i + CHUNKSIZE], min_limit, max_limit] for i in range(0, Xdata.size, CHUNKSIZE)],
+            ):
                 min_val = min(_min, min_val)
                 max_val = max(_max, max_val)
 
     else:
-        min_val, max_val = min_max(Xdata)
+        min_val, max_val = min_max(Xdata, min_limit, max_limit)
 
     excess_range = (max_val - min_val) > 24
     return XApproxDistribution.COUNT if excess_range else XApproxDistribution.NORMAL
