bee02c0a1dba2229e8b62a3601763d370dfd677f
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-08-09 17:31:39 -0700

    simple typings

--- client/src/util/renderThrottle.ts
+++ client/src/util/renderThrottle.ts
@@ -1,17 +1,16 @@
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types, @typescript-eslint/no-explicit-any -- - FIXME: disabled temporarily on migrate to TS.
-export default function renderThrottle(callback: any) {
+export default function renderThrottle(
+  callback: () => void
+): (this: unknown) => void {
   /*
   This wraps a call to requestAnimationFrame(), enforcing a single
   render callback at any given time (ie, you can call this any number
   of times, and it will coallesce multiple inter-frame calls into a
   single render).
   */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any --- FIXME: disabled temporarily on migrate to TS.
-  let rafCurrentlyInProgress: any = null;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any --- FIXME: disabled temporarily on migrate to TS.
-  return function f(this: any) {
+  let rafCurrentlyInProgress: number | null = null;
+  return function f(this: unknown) {
     if (rafCurrentlyInProgress) return;
-    // eslint-disable-next-line @typescript-eslint/no-this-alias --- FIXME: disabled temporarily on migrate to TS.
+    // eslint-disable-next-line @typescript-eslint/no-this-alias --- required for functionality
     const context = this;
     rafCurrentlyInProgress = window.requestAnimationFrame(() => {
       callback.apply(context);
