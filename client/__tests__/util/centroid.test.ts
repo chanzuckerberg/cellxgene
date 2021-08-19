3c31b57b0798469a37c739516cc51961c4a4c7a4
bkmartinjr <bruce@chanzuckerberg.com>
2021-08-19 14:56:24 -0700

    Revert "API update for tests (#2354)"
    This reverts commit 97fb98b4eb9095835b7748dca79f7df91d2ae5cf.

--- client/__tests__/util/centroid.test.ts
+++ client/__tests__/util/centroid.test.ts
@@ -5,7 +5,7 @@ import quantile from "../../src/util/quantile";
 import { matrixFBSToDataframe } from "../../src/util/stateManager/matrix";
 import * as REST from "./stateManager/sampleResponses";
 import { indexEntireSchema } from "../../src/util/stateManager/schemaHelpers";
-import { normalizeWritableCategoricalSchema } from "../../src/annoMatrix/normalize";
+import { _normalizeCategoricalSchema } from "../../src/annoMatrix/schema";
 
 describe("centroid", () => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any --- FIXME: disabled temporarily on migrate to TS.
@@ -20,7 +20,7 @@ describe("centroid", () => {
     obsAnnotations = matrixFBSToDataframe(REST.annotationsObs);
     obsLayout = matrixFBSToDataframe(REST.layoutObs);
 
-    normalizeWritableCategoricalSchema(
+    _normalizeCategoricalSchema(
       schema.annotations.obsByName.field3,
       obsAnnotations.col("field3")
     );
