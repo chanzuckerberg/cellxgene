251bb6ba2aa48c030b8725d86d42e422caff35af
bkmartinjr <bruce@chanzuckerberg.com>
2021-08-20 11:26:36 -0700

    add additional babel config

--- client/configuration/babel/babel.dev.js
+++ client/configuration/babel/babel.dev.js
@@ -17,6 +17,7 @@ module.exports = {
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
     ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
     "@babel/plugin-proposal-export-namespace-from",
     "@babel/plugin-proposal-optional-chaining",
     "@babel/plugin-proposal-nullish-coalescing-operator",
--- client/configuration/babel/babel.prod.js
+++ client/configuration/babel/babel.prod.js
@@ -16,6 +16,7 @@ module.exports = {
     ["@babel/plugin-proposal-decorators", { legacy: true }],
     ["@babel/plugin-proposal-class-properties", { loose: true }],
     ["@babel/plugin-proposal-private-methods", { loose: true }],
+    ["@babel/plugin-proposal-private-property-in-object", { loose: true }],
     "@babel/plugin-proposal-export-namespace-from",
     "@babel/plugin-transform-react-constant-elements",
     "@babel/plugin-transform-runtime",
