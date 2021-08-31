ffa18b64d4d26bbf2f55464dfbbc239c5529864d
Seve Badajoz <sbadajoz@chanzuckerberg.com>
2021-08-31 15:30:13 -0700

    Bump version: 0.18.0-rc.0 → 0.18.0

--- backend/server/__init__.py
+++ backend/server/__init__.py
@@ -2,7 +2,7 @@ import logging
 import sys
 from backend.common.utils.utils import import_plugins
 
-__version__ = "0.18.0-rc.0"
+__version__ = "0.18.0"
 display_version = "cellxgene v" + __version__
 
 try:
