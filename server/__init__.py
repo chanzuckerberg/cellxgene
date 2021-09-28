f4986c9c7de5d1fa00e154c82054b18a8d901488
Ben MR <bmasscheleinrodgers@chanzuckerberg.com>
2021-09-28 14:10:24 -0400

    Release version 0.19.0 (#2444)
    * Bump version: 0.18.0 → 0.19.0-rc.0
    
    * release can 0.19.0
    
    * Bump version: 0.19.0-rc.0 → 0.19.0-rc.1
    
    * bump rc
    
    * Bump version: 0.19.0-rc.1 → 0.19.0
    
    * release final

--- server/__init__.py
+++ server/__init__.py
@@ -2,7 +2,7 @@ import logging
 import sys
 from server.common.utils.utils import import_plugins
 
-__version__ = "0.18.0"
+__version__ = "0.19.0"
 display_version = "cellxgene v" + __version__
 
 try:
