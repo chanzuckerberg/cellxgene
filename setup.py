591bf3659161b518bcf5c46b4b6d83b89adda84d
atarashansky <alexander.tarashansky@atarashansky-mba.local>
2021-10-11 12:02:07 -0700

    up

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("backend/server/requirements-prepare.txt") as fh:
 
 setup(
     name="excellxgene",
-    version="1.0.2",
+    version="1.0.3",
     packages=find_packages(),
     url="https://github.com/czbiohub/cellxgene",
     license="MIT",
