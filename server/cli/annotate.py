cfcfd009107fed830f2d252265797ee6127fec85
Andrew Tolopko <atolopko@chanzuckerberg.com>
2022-05-19 13:31:14 -0400

    enable cli annotate subcmd

--- server/cli/annotate.py
+++ server/cli/annotate.py
@@ -1,7 +1,13 @@
 import functools
+import pickle
 import sys
+from typing import Any
 
 import click
+import scanpy
+
+from server.annotate import cell_type
+from server.common.utils.data_locator import DataLocator
 
 from server.common.utils.utils import sort_options
 
@@ -12,6 +18,8 @@ def annotate_args(func):
         return func(*args, **kwargs)
 
     return wrapper
+
+
 
 
 @sort_options
@@ -38,7 +46,7 @@ def annotate_args(func):
          "raw counts will be read from `X` matrix, unless 'raw.X' exists, in which case that will be used."
 
 )
-# TODO: Useful if we want to support multiple tissue types (for cell type annotation models)
+# TODO: Useful if we want to support discoverability of models
 # @click.option(
 #     "-r",
 #     "--model-repository",
@@ -60,14 +68,15 @@ def annotate_args(func):
     help="A prefix used to form the names of new `obs` annotation columns that will store the predicted annotation "
          "values and confidence scores."
 )
-# @click.option(
-#     "-s",
-#     "--annotation-column-suffix",
-#     type=str,
-#     help="An optional suffix used to form the names of new `obs` annotation columns that will store the predicted "
-#          "annotation values and confidence scores. This can be used to allow multiple annotation predictions to be "
-#          "run on a single AnnData object."
-# )
+@click.option(
+    "-n",
+    "--run-name",
+    "--annotation-column-suffix",
+    type=str,
+    help="An optional suffix used to form the names of new `obs` annotation columns that will store the predicted "
+         "annotation values and confidence scores. This can be used to allow multiple annotation predictions to be "
+         "run on a single AnnData object."
+)
 @click.option(
     "-u",
     "--update-h5ad-file",
@@ -85,6 +94,20 @@ def annotate_args(func):
 def annotate(**cli_args):
     print(cli_args)
 
+    _validate_options(cli_args)
+
+    query_dataset = scanpy.read_h5ad(cli_args['input_h5ad_file'])
+    model = _fetch_model(cli_args['model_url'])
+
+    cell_type.annotate(query_dataset, model, annotation_column_name_prefix=cli_args['annotation_column_prefix'])
+
+    # If no --output-h5ad-file option, this will write back to the input file
+    query_dataset.write_h5ad(cli_args['output_h5ad_file'])
+
+    print("added annotations to {query_dataset.filename}")
+
+
+def _validate_options(cli_args):
     # TODO(atolopko): Use cloup library for this logic
     if cli_args["update_h5ad_file"] and cli_args["output_h5ad_file"]:
         click.echo("--update_h5ad_file and --output_h5ad_file are mutually exclusive")
@@ -92,6 +115,15 @@ def annotate(**cli_args):
     if not (cli_args["update_h5ad_file"] or cli_args["output_h5ad_file"]):
         click.echo("--update_h5ad_file or --output_h5ad_file must be specified")
         sys.exit(1)
+
+
+def _fetch_model(model_url) -> Any:
+    model_url_locator = DataLocator(model_url)
+    if not model_url_locator.exists():
+        raise f"model file '{model_url}' not found"
+
+    with model_url_locator.open() as f:
+        return pickle.load(f)
 
 
 if __name__ == "__main__":
