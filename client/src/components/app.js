e72ee55010f49e6a4232a62e5a4bc59bd5d061c5
ashin-czi <ashin@contractor.chanzuckerberg.com>
2022-08-05 11:53:23 -0400

    docs: changing page titles to "CELLxGENE | Annotate"

--- client/src/components/app.js
+++ client/src/components/app.js
@@ -41,7 +41,7 @@ class App extends React.Component {
     const { loading, error, graphRenderCounter } = this.props;
     return (
       <Container>
-        <Helmet title="cellxgene" />
+        <Helmet title="CELLxGENE | Annotate" />
         {loading ? (
           <div
             style={{
