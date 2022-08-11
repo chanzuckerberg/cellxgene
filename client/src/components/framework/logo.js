ddf58a957242a098c2a2a677a0b97999694b0426
ashin-czi <ashin@contractor.chanzuckerberg.com>
2022-08-11 15:22:11 -0400

    updating svg logo icon for rebrand

--- client/src/components/framework/logo.js
+++ client/src/components/framework/logo.js
@@ -6,11 +6,10 @@ const Logo = (props) => {
   return (
     <svg width={size} height={size} viewBox="0 0 48 48" fill="none">
       <rect width="48" height="48" fill="white" />
-      <rect width="48" height="48" fill={globals.logoColor} />
-      <rect x="19" y="19" width="22" height="22" fill="white" />
-      <rect x="24" y="24" width="12" height="12" fill={globals.logoColor} />
-      <rect x="7" y="19" width="7" height="22" fill="white" />
-      <rect x="19" y="7" width="22" height="7" fill="white" />
+      <rect x="14" y="14" width="34" height="34" fill={globals.logoColor} />
+      <rect x="22" y="22" width="18" height="18" fill="white" />
+      <rect x="0" y="14" width="8" height="34" fill={globals.logoColor} />
+      <rect x="14" y="0" width="34" height="8" fill={globals.logoIconAccentColor} />
     </svg>
   );
 };
