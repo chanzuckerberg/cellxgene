431ae0ed034f561ceaabc92125bcdd294530537e
Seve Badajoz <severiano.badajoz@chanzuckerberg.com>
2022-08-23 09:45:34 -0700

    Bump version: 1.1.0-rc.1 → 1.1.0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open("server/requirements-annotate.txt") as fh:
 
 setup(
     name="cellxgene",
-    version="1.1.0-rc.1",
+    version="1.1.0",
     packages=find_packages(),
     url="https://github.com/chanzuckerberg/cellxgene",
     license="MIT",
