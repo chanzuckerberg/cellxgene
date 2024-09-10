02bbca25dc3f70a8692da114bcd1ca97de6867f0
Suzette McCanny <smccanny@deloitte.ca>
2024-09-10 12:28:44 -0400

    add timeout before setup native

--- client/__tests__/globalSetup.js
+++ client/__tests__/globalSetup.js
@@ -2,6 +2,5 @@ const { setup } = require("jest-environment-puppeteer");
 
 module.exports = async () => {
   console.log("Global setup...");
-  jest.setTimeout(20 * 1000);
-  await setup();
+  setTimeout(setup, 30000);
 };
